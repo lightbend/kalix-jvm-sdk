fc221636a8938a95d21ae4c0022b579835ca88e8
Rory Stokes <contact@rorystok.es>
2021-01-06 22:20:20 +1030

    maven-java: Added basic archetype based on example, ensured successful compilation

---
+++ maven-java/src/main/resources/archetype-resources/src/main/java/MyServiceEntity.java
@@ -0,0 +1,55 @@
+package $package;
+
+import ${package}.Myentity;
+import ${package}.persistence.Domain;
+import com.google.protobuf.Empty;
+import io.cloudstate.javasupport.EntityId;
+import io.cloudstate.javasupport.eventsourced.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/** An event sourced entity. */
+@EventSourcedEntity
+public class MyServiceEntity {
+  private final String entityId;
+  private int value = 0;
+
+  public MyServiceEntity(@EntityId String entityId) {
+    this.entityId = entityId;
+  }
+
+  /**
+  * This method will be called when snapshot is created
+  */
+  @Snapshot
+  public Myentity.MyState snapshot() {
+    return Myentity.MyState.newBuilder()
+        .setValue(this.value)
+        .build();
+  }
+
+  @SnapshotHandler
+  public void handleSnapshot(Myentity.MyState state) {
+    this.value = state.getValue();
+  }
+
+  @EventHandler
+  public void valueSet(Domain.ValueSet event) {
+    this.value = event.getValue();
+  }
+
+  @CommandHandler
+  public Empty set(Myentity.SetValue setValue, CommandContext commandContext) {
+    this.value = setValue.getValue();
+
+    return Empty.getDefaultInstance();
+  }
+
+  @CommandHandler
+  public Myentity.MyState get(Myentity.GetValue getValue, CommandContext commandContext) {
+    return Myentity.MyState.newBuilder()
+        .setValue(value)
+        .build();
+  }
+}
