cdde71446de80e5a99bcb74640f28d3065edb07b
Rory Stokes <contact@rorystok.es>
2021-01-07 18:00:17 +1030

    Add documentation

--- src/main/resources/archetype-resources/src/main/java/MyServiceEntity.java
+++ src/main/resources/archetype-resources/src/main/java/MyServiceEntity.java
@@ -6,9 +6,9 @@ import io.cloudstate.javasupport.EntityId;
 import io.cloudstate.javasupport.eventsourced.*;
 
 /** An event sourced entity. */
-/** An event sourced entity. */
 @EventSourcedEntity
 public class MyServiceEntity {
+  @SuppressWarnings("unused")
   private final String entityId;
 
   private int value = 0;
@@ -30,17 +30,34 @@ public class MyServiceEntity {
     this.value = state.getValue();
   }
 
+  /**
+   * Handle the ValueSet event, updating the persisted value
+   * @param event the event payload containing the updated value
+   */
   @EventHandler
   public void valueSet(Domain.ValueSet event) {
     this.value = event.getValue();
   }
 
+  /**
+   * Handle the SetValue command
+   * Dispatches a ValueSet event with the provided value.
+   * @param command the command payload, containing the value to update to
+   * @param commandContext the Akka Serverless command context
+   */
   @CommandHandler
   public Empty setValue(MyEntity.SetValueCommand command, CommandContext commandContext) {
     commandContext.emit(Domain.ValueSet.newBuilder().setValue(command.getValue()).build());
     return Empty.getDefaultInstance();
   }
 
+  /**
+   * Handle the GetValue command
+   * Returns a state payload with the persisted value.
+   * @param command the command payload
+   * @param commandContext the Akka Serverless command context
+   * @return a MyState payload conatining the persisted value
+   */
   @CommandHandler
   public MyEntity.MyState getValue(MyEntity.GetValueCommand command, CommandContext commandContext) {
     return MyEntity.MyState.newBuilder().setValue(this.value).build();
