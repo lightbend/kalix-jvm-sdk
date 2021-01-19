7cd5075f15419bc7bbbc98c23ce9d51ad253391f
huntc <huntchr@gmail.com>
2021-01-19 11:45:47 +1100

    Debugging

--- src/test/java/com/lightbend/GenerateMojoTest.java
+++ src/test/java/com/lightbend/GenerateMojoTest.java
@@ -6,6 +6,7 @@ import org.apache.maven.plugin.testing.MojoRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -34,6 +35,9 @@ public class GenerateMojoTest {
         GenerateMojo myMojo = (GenerateMojo) rule.lookupConfiguredMojo(projectDirectory.toFile(), "generate");
         myMojo.execute();
 
+        for (File file : FileUtils.listFiles(projectDirectory.toFile(), new String[]{"java"}, true)) {
+            System.out.println(file.toString());
+        }
         assertTrue(projectDirectory.resolve("src/main/java/com/lightbend/MyService.java").toFile().exists());
     }
 }
