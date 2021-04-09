1cfcb1e9c6b3230b4fab475a3ae588f65cf64375
huntc <huntchr@gmail.com>
2021-04-09 12:37:50 +1000

    codegen: Makes GraalVM happy
    GraalVM native-image couldn't resolve a reflection lookup. The reflection lookup itself isn't on our execution path as it is for something unshaded, so we are able to safely declare the target class.

---
+++ codegen/js-gen-cli/src/main/scala/io/netty/channel/socket/nio/NioServerSocketChannel.scala
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Lightbend Inc. 2021
+ *
+ * Dummy class for gRPC reflection and native image building - which isn't being used.
+ */
+
+package io.netty.channel.socket.nio
+
+class NioServerSocketChannel {}
