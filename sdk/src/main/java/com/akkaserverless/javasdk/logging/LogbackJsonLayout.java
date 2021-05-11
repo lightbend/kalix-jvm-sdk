b02bcdc0bbb571dac0a7af36029aa73b850d1241
Enno Runne <458526+ennru@users.noreply.github.com>
2021-05-11 09:04:39 +0200

    Java SDK: add support class for JSON formatted logging

---
+++ sdk/src/main/java/com/akkaserverless/javasdk/logging/LogbackJsonLayout.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021 Lightbend Inc.
+ */
+
+package com.akkaserverless.javasdk.logging;
+
+/**
+ * This Logback JSON layout uses the name `severity` (instead of `level`).
+ *
+ * <p>Eg. Stackdriver expects the log severity to be in a field called `severity`.
+ *
+ * <p>IMPORTANT: This class depends on the "logback-json-classic" library (organization
+ * "ch.qos.logback.contrib") and the Jackson layout support requires "logback-jackson" (organization
+ * "ch.qos.logback.contrib") which need to be added as dependencies.
+ */
+public final class LogbackJsonLayout extends ch.qos.logback.contrib.json.classic.JsonLayout {
+
+  public LogbackJsonLayout() {
+    setIncludeLevel(false);
+  }
+
+  @Override
+  public void addCustomDataToJsonMap(
+      java.util.Map<String, Object> map, ch.qos.logback.classic.spi.ILoggingEvent event) {
+    add("severity", true, String.valueOf(event.getLevel()), map);
+  }
+}
