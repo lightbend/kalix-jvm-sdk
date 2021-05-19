e77c3bc02f855e116c58e89bfc49015ac965436f
Rory Stokes <contact@rorystok.es>
2021-05-19 22:29:55 +0930

    Add Java value entity tests

--- java-gen/src/it/scala/com/lightbend/akkasls/codegen/java/JavaCodegenIntegrationSuite.scala
+++ java-gen/src/it/scala/com/lightbend/akkasls/codegen/java/JavaCodegenIntegrationSuite.scala
@@ -62,8 +62,8 @@ class JavaCodegenIntegrationSuite extends munit.FunSuite {
   // Start the codegen container (the proxy fails its health checks until a service is available)
   codegenContainer.start()
 
-  test("verify unmodified generated entity") {
-    val entityName = "unmodified-entity"
+  test("verify unmodified generated event sourced entity") {
+    val entityName = "unmodified-eventsourced-entity"
 
     // Generate a new entity within the codegen container
     assertSuccessful(generateEntity(codegenContainer)(entityName))
@@ -102,8 +102,8 @@ class JavaCodegenIntegrationSuite extends munit.FunSuite {
     proxyContainer.stop()
   }
 
-  test("verify simple implementation from generated entity") {
-    val entityName = "simple-impl-entity"
+  test("verify simple implementation of event sourced entity") {
+    val entityName = "simple-impl-eventsourced-entity"
 
     // Generate a new entity within the codegen container
     assertSuccessful(generateEntity(codegenContainer)(entityName))
@@ -146,6 +146,164 @@ class JavaCodegenIntegrationSuite extends munit.FunSuite {
             case """        throw new RuntimeException("The event handler for `ValueSet` is not implemented, yet");""" =>
               Seq(
                 """        value = event.getValue();"""
+              )
+            case line => Seq(line)
+          }
+          .foreach { line =>
+            println(line)
+            writer.write(s"${line}\n")
+          }
+      )
+    }
+    codegenContainer.copyFileToContainer(MountableFile.forHostPath(implFile), implContainerPath)
+
+    // Start the entity gRPC server
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/start-entity.sh",
+        entityName
+      )
+    )
+
+    // Start the proxy
+    proxyContainer.start()
+    val proxyUrl = s"http://${proxyContainer.getHost}:${proxyContainer.getMappedPort(9000)}"
+
+    // Eventually, a GetValue request should return a 200 OK
+    var getResult = retryUntil[requests.Response](_.statusCode == 200) {
+      requests.post(
+        s"$proxyUrl/com.example.MyServiceEntity/GetValue",
+        check = false,
+        data = """{"entityId": "test-entity-id"}""",
+        headers = Map("Content-Type" -> "application/json")
+      )
+    }
+    assertEquals(getResult.statusCode, 200)
+    assertEquals(getResult.text(), """{"value":0}""")
+
+    val newValue = 42;
+
+    val setResult = requests.post(
+      s"$proxyUrl/com.example.MyServiceEntity/SetValue",
+      check = false,
+      data = s"""{"entityId": "test-entity-id", "value": ${newValue}}""",
+      headers = Map("Content-Type" -> "application/json")
+    )
+    assertEquals(setResult.statusCode, 200)
+
+    val getAgainResult =
+      requests.post(
+        s"$proxyUrl/com.example.MyServiceEntity/GetValue",
+        check = false,
+        data = """{"entityId": "test-entity-id"}""",
+        headers = Map("Content-Type" -> "application/json")
+      )
+    assertEquals(getAgainResult.statusCode, 200)
+    assertEquals(getAgainResult.text(), s"""{"value":${newValue}}""")
+
+    // Kill the gRPC server, and stop the proxy
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/stop-entity.sh",
+        entityName
+      )
+    )
+    proxyContainer.stop()
+  }
+
+  test("verify unmodified generated value entity") {
+    val entityName = "unmodified-value-entity"
+
+    // Generate a new entity within the codegen container
+    assertSuccessful(generateEntity(codegenContainer)(entityName))
+
+    // Overwrite the domain with a value entity definition
+    codegenContainer.copyFileToContainer(
+      MountableFile.forClasspathResource("proto/value-entity-domain.proto"),
+      s"/home/$entityName/src/main/proto/persistence/domain.proto"
+    )
+
+    // Start the entity gRPC server
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/start-entity.sh",
+        entityName
+      )
+    )
+
+    // Start the proxy
+    proxyContainer.start()
+    val proxyUrl = s"http://${proxyContainer.getHost}:${proxyContainer.getMappedPort(9000)}"
+
+    // Eventually, a GetValue request should return a 500 error, with our generated error message
+    var getResult = retryUntil[requests.Response](_.statusCode == 500) {
+      requests.post(
+        s"$proxyUrl/com.example.MyServiceEntity/GetValue",
+        check = false,
+        data = """{"entityId": "test-entity-id"}""",
+        headers = Map("Content-Type" -> "application/json")
+      )
+    }
+    assertEquals(getResult.statusCode, 500)
+    assertEquals(getResult.text(), "The command handler for `GetValue` is not implemented, yet")
+
+    // Kill the gRPC server, and stop the proxy
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/stop-entity.sh",
+        entityName
+      )
+    )
+    proxyContainer.stop()
+  }
+
+  test("verify simple implementation of value sourced entity") {
+    val entityName = "simple-impl-value-entity"
+
+    // Generate a new entity within the codegen container
+    assertSuccessful(generateEntity(codegenContainer)(entityName))
+
+    // Overwrite the domain with a value entity definition
+    codegenContainer.copyFileToContainer(
+      MountableFile.forClasspathResource("proto/value-entity-domain.proto"),
+      s"/home/$entityName/src/main/proto/persistence/domain.proto"
+    )
+
+    // Build the initial implementation
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "bash",
+        "-c",
+        s"cd ${entityName} && mvn compile"
+      )
+    )
+
+    // Stream generated MyEntityImpl, and replace function bodies with simple implementations
+    val implFile = Files.createTempFile("generated-entity-impl", ".java")
+    val implContainerPath =
+      s"/home/${entityName}/src/main/java/com/example/persistence/MyEntityImpl.java"
+    Using(new PrintWriter(implFile.toFile())) { writer =>
+      codegenContainer.copyFileFromContainer(
+        implContainerPath,
+        Source
+          .fromInputStream(_)
+          .getLines()
+          .flatMap {
+            case """    private final String entityId;""" =>
+              Seq(
+                """    private final String entityId;""",
+                """    private int value;"""
+              )
+            case """        throw ctx.fail("The command handler for `GetValue` is not implemented, yet");""" =>
+              Seq(
+                """        Domain.MyState state = ctx.getState().orElse(Domain.MyState.newBuilder().build());""",
+                """        MyEntityApi.MyResult result = MyEntityApi.MyResult.newBuilder().setValue(state.getValue()).build();""",
+                """        return result;"""
+              )
+            case """        throw ctx.fail("The command handler for `SetValue` is not implemented, yet");""" =>
+              Seq(
+                """        ctx.updateState(Domain.MyState.newBuilder().setValue(command.getValue()).build());""",
+                """        return Empty.getDefaultInstance();"""
               )
             case line => Seq(line)
           }
