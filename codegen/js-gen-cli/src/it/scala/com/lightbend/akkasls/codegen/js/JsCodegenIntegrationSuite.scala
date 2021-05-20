e58bbe7ca2f329ab4798f68c52d6b3da746d383d
Rory Stokes <contact@rorystok.es>
2021-05-20 12:15:17 +0930

    codegen: Add JS value entity tests, fix stop script not terminating entity

--- codegen/js-gen-cli/src/it/scala/com/lightbend/akkasls/codegen/js/JsCodegenIntegrationSuite.scala
+++ codegen/js-gen-cli/src/it/scala/com/lightbend/akkasls/codegen/js/JsCodegenIntegrationSuite.scala
@@ -61,8 +61,8 @@ class JsCodegenIntegrationSuite extends munit.FunSuite {
   // Start the codegen container (the proxy fails its health checks until a service is available)
   codegenContainer.start()
 
-  test("verify unmodified generated entity") {
-    val entityName = "unmodified-entity"
+  test("verify unmodified generated event-sourced entity") {
+    val entityName = "unmodified-eventsourced-entity"
 
     // Generate a new entity within the codegen container
     assertSuccessful(
@@ -110,8 +110,8 @@ class JsCodegenIntegrationSuite extends munit.FunSuite {
     proxyContainer.stop()
   }
 
-  test("verify simple implementation from generated entity") {
-    val entityName = "simple-impl-entity"
+  test("verify simple implementation of event-sourced entity") {
+    val entityName = "simple-impl-eventsourced-entity"
 
     // Generate a new entity within the codegen container
     assertSuccessful(
@@ -172,6 +172,180 @@ class JsCodegenIntegrationSuite extends munit.FunSuite {
 
     // Eventually, a GetValue request should return a 200 OK
     var getResult = retryUntil[requests.Response](_.statusCode == 200) {
+      requests.post(
+        s"$proxyUrl/com.example.MyServiceEntity/GetValue",
+        check = false,
+        data = """{"entityId": "test-entity-id"}""",
+        headers = Map("Content-Type" -> "application/json")
+      )
+    }
+    assertEquals(getResult.statusCode, 200)
+    assertEquals(getResult.text(), """{"value":0}""")
+
+    val newValue = 42;
+
+    val setResult = requests.post(
+      s"$proxyUrl/com.example.MyServiceEntity/SetValue",
+      check = false,
+      data = s"""{"entityId": "test-entity-id", "value": ${newValue}}""",
+      headers = Map("Content-Type" -> "application/json")
+    )
+    assertEquals(setResult.statusCode, 200)
+
+    val getAgainResult =
+      requests.post(
+        s"$proxyUrl/com.example.MyServiceEntity/GetValue",
+        check = false,
+        data = """{"entityId": "test-entity-id"}""",
+        headers = Map("Content-Type" -> "application/json")
+      )
+    assertEquals(getAgainResult.statusCode, 200)
+    assertEquals(getAgainResult.text(), s"""{"value":${newValue}}""")
+
+    // Kill the gRPC server, and stop the proxy
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/stop-entity.sh",
+        entityName
+      )
+    )
+    proxyContainer.stop()
+  }
+
+  test("verify unmodified generated value entity") {
+    val entityName = "unmodified-value-entity"
+
+    // Generate a new entity within the codegen container
+    assertSuccessful(
+      codegenContainer.execInContainer("create-akkasls-entity", entityName)
+    )
+
+    // Overwrite the domain with a value entity definition
+    codegenContainer.copyFileToContainer(
+      MountableFile.forClasspathResource("proto/value-entity-domain.proto"),
+      s"/home/$entityName/proto/persistence/domain.proto"
+    )
+
+    // Setup and build the entity
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/setup-entity.sh",
+        entityName
+      )
+    )
+
+    // Start the entity gRPC server
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/start-entity.sh",
+        entityName
+      )
+    )
+
+    // Start the proxy
+    proxyContainer.start()
+    val proxyUrl = s"http://${proxyContainer.getHost}:${proxyContainer.getMappedPort(9000)}"
+
+    // Eventually, a GetValue request should return a 500 error, with our generated error message
+    var getResult = retryUntil[requests.Response] { r =>
+      println(r.toString())
+      r.statusCode == 500
+    } {
+      requests.post(
+        s"$proxyUrl/com.example.MyServiceEntity/GetValue",
+        check = false,
+        data = """{"entityId": "test-entity-id"}""",
+        headers = Map("Content-Type" -> "application/json")
+      )
+    }
+    assertEquals(getResult.statusCode, 500)
+    assertEquals(getResult.text(), "The command handler for `GetValue` is not implemented, yet")
+
+    // Kill the gRPC server, and stop the proxy
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/stop-entity.sh",
+        entityName
+      )
+    )
+    proxyContainer.stop()
+  }
+
+  test("verify simple implementation of value entity") {
+    val entityName = "simple-impl-value-entity"
+
+    // Generate a new entity within the codegen container
+    assertSuccessful(
+      codegenContainer.execInContainer("create-akkasls-entity", entityName)
+    )
+
+    // Overwrite the domain with a value entity definition
+    codegenContainer.copyFileToContainer(
+      MountableFile.forClasspathResource("proto/value-entity-domain.proto"),
+      s"/home/$entityName/proto/persistence/domain.proto"
+    )
+
+    // Setup and build the entity
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/setup-entity.sh",
+        entityName
+      )
+    )
+
+    // Stream generated MyEntityImpl, and replace function bodies with simple implementations
+    val implFile = Files.createTempFile("generated-entity-impl", ".js")
+    val implContainerPath =
+      s"/home/${entityName}/src/myserviceentity.js"
+    Using(new PrintWriter(implFile.toFile())) { writer =>
+      codegenContainer.copyFileFromContainer(
+        implContainerPath,
+        Source
+          .fromInputStream(_)
+          .getLines()
+          .flatMap {
+            case """    return ctx.fail("The command handler for `GetValue` is not implemented, yet");""" =>
+              Seq(
+                """    return state;"""
+              )
+            case """    return ctx.fail("The command handler for `SetValue` is not implemented, yet");""" =>
+              Seq(
+                """    ctx.updateState({ type: "MyState", value: command.value });""",
+                """    return {};"""
+              )
+            case line => Seq(line)
+          }
+          .foreach { line =>
+            println(line)
+            writer.write(s"${line}\n")
+          }
+      )
+    }
+    codegenContainer.copyFileToContainer(MountableFile.forHostPath(implFile), implContainerPath)
+
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "ps"
+      )
+    )
+
+    // Start the entity gRPC server
+    assertSuccessful(
+      codegenContainer.execInContainer(
+        "./scripts/start-entity.sh",
+        entityName
+      )
+    )
+
+    // Start the proxy
+    proxyContainer.start()
+    val proxyUrl = s"http://${proxyContainer.getHost}:${proxyContainer.getMappedPort(9000)}"
+
+    // Eventually, a GetValue request should return a 200 OK
+    var getResult = retryUntil[requests.Response] { r =>
+      println(r.toString())
+      r.statusCode == 200
+    } {
       requests.post(
         s"$proxyUrl/com.example.MyServiceEntity/GetValue",
         check = false,
