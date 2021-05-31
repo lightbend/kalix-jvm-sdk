03e2783b8e359a61dbe166041ff247724ab11e2d
huntc <huntchr@gmail.com>
2021-05-31 12:33:43 +1000

    codegen: Removes the global
    Removes the global statically initialised service declaration. Not needed, particularly during testing.

--- codegen/java-gen/src/test/scala/com/lightbend/akkasls/codegen/java/SourceGeneratorSuite.scala
+++ codegen/java-gen/src/test/scala/com/lightbend/akkasls/codegen/java/SourceGeneratorSuite.scala
@@ -703,12 +703,10 @@ class SourceGeneratorSuite extends munit.FunSuite {
         |    
         |    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
         |    
-        |    public static final AkkaServerless SERVICE =
-        |            withGeneratedComponentsAdded(new AkkaServerless());
-        |    
         |    public static void main(String[] args) throws Exception {
         |        LOG.info("starting the Akka Serverless service");
-        |        SERVICE.start().toCompletableFuture().get();
+        |        withGeneratedComponentsAdded(new AkkaServerless())
+        |                .start().toCompletableFuture().get();
         |    }
         |}""".stripMargin
     )
