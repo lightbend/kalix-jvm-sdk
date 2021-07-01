56cb7a8320a262b00a0c4e95f2fa94ef3a644f11
Arnout Engelen <arnout@bzzt.net>
2021-07-01 11:56:30 +0200

    First pass using codegen instead of reflection
    Manually create a `CustomerValueEntityHandler` that would be generated
    as part of codegen.
    
    Added basic code to hook it into `Main`, but that API would still change
    and partly be generated.
    
    Reading the codegen code, it's clear we're losing some features that
    used to be available with codegen, marked them with TODO's - we should
    consider whether we're OK with losing them or want to introduce new
    mechanisms.

---
+++ samples/java-customer-registry/src/main/java/customer/CustomerValueEntityInterface.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Lightbend Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package customer;
+
+import com.akkaserverless.javasdk.valueentity.CommandContext;
+import com.akkaserverless.javasdk.valueentity.ValueEntityBase;
+import com.google.protobuf.Empty;
+import customer.api.CustomerApi;
+import customer.domain.CustomerDomain;
+
+import java.util.Optional;
+
+public abstract class CustomerValueEntityInterface
+    extends ValueEntityBase<CustomerDomain.CustomerState> {
+
+  public abstract Effect<CustomerApi.Customer> getCustomer(
+      CustomerApi.GetCustomerRequest request,
+      CustomerDomain.CustomerState currentState,
+      CommandContext<CustomerDomain.CustomerState> context);
+
+  public abstract Effect<Empty> create(
+      CustomerApi.Customer customer,
+      CustomerDomain.CustomerState currentState,
+      CommandContext<CustomerDomain.CustomerState> context);
+
+  public abstract Effect<Empty> changeName(
+      CustomerApi.ChangeNameRequest request,
+      CustomerDomain.CustomerState currentState,
+      CommandContext<CustomerDomain.CustomerState> context);
+}
