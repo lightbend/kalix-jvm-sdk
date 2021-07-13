4b2e9b73277d2ddb90353523f43c36998b396f8f
Arnout Engelen <arnout@bzzt.net>
2021-07-13 13:13:57 +0000

    Add script to publish codegen and maven plugins locally
    For easier testing

---
+++ publishLocalM2.sh
@@ -0,0 +1,15 @@
+# publish sbt and maven artifacts
+sbt publishM2
+SDK_VERSION=$(sbt "print sdk/version" | tail -1)
+cd maven-java
+mvn versions:set -DnewVersion=$SDK_VERSION
+mvn install
+
+# cleanup
+rm pom.xml.versionsBackup
+rm */pom.xml.versionsBackup
+git checkout pom.xml
+git checkout */pom.xml
+
+# tell the user what to do next
+echo now you execute: export SDK_VERSION=$SDK_VERSION
