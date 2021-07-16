c646604259ba2bb0d0cb82f7b8366709e80555d8
Renato Cavalcanti <renato@cavalcanti.be>
2021-07-16 22:29:53 +0200

    fix compilation

--- samples/valueentity-counter/src/main/java/com/example/MainComponentRegistrations2.java
+++ samples/valueentity-counter/src/main/java/com/example/MainComponentRegistrations2.java
@@ -2,7 +2,7 @@ package com.example;
 
 import com.akkaserverless.javasdk.AkkaServerless;
 import com.example.domain.CounterDomain;
-import com.example.domain.CounterImpl;
+import com.example.domain.Counter;
 import com.google.protobuf.EmptyProto;
 
 public final class MainComponentRegistrations2 {
@@ -10,7 +10,7 @@ public final class MainComponentRegistrations2 {
     public static AkkaServerless withGeneratedComponentsAdded(AkkaServerless akkaServerless) {
         return akkaServerless
                 .registerValueEntity(
-                    CounterImpl.class,
+                    Counter.class,
                     CounterApi.getDescriptor().findServiceByName("CounterService"),
                     CounterDomain.getDescriptor(),
                     EmptyProto.getDescriptor()
