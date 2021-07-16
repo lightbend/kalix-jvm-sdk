dd346c08d0894db114267f721432df05070f128c
Renato Cavalcanti <renato@cavalcanti.be>
2021-07-16 22:29:53 +0200

    fix compilation

--- samples/java-eventsourced-shopping-cart/src/main/java/com/example/shoppingcart/MainComponentRegistrations2.java
+++ samples/java-eventsourced-shopping-cart/src/main/java/com/example/shoppingcart/MainComponentRegistrations2.java
@@ -18,14 +18,14 @@ package com.example.shoppingcart;
 
 import com.akkaserverless.javasdk.AkkaServerless;
 import com.example.shoppingcart.domain.ShoppingCartDomain;
-import com.example.shoppingcart.domain.ShoppingCartImpl;
+import com.example.shoppingcart.domain.ShoppingCart;
 import com.google.protobuf.EmptyProto;
 
 public final class MainComponentRegistrations2 {
 
   public static AkkaServerless withGeneratedComponentsAdded(AkkaServerless akkaServerless) {
     return akkaServerless.registerEventSourcedEntity(
-        ShoppingCartImpl.class,
+        ShoppingCart.class,
         ShoppingCartApi.getDescriptor().findServiceByName("ShoppingCartService"),
         ShoppingCartDomain.getDescriptor(),
         EmptyProto.getDescriptor());
