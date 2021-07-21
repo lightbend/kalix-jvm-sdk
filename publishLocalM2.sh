ce3199ea9b1725c7673830585f7027f4cc03e3b8
Arnout Engelen <arnout@bzzt.net>
2021-07-21 15:24:54 +0000

    feat: add some logging to codegen
    Notably for the case where there is an entity service,
    but no entity component configuration that matches by
    the component name - in which case nothing would be
    generated, silently.

--- publishLocalM2.sh
+++ publishLocalM2.sh
@@ -1,5 +1,21 @@
-# publish sbt and maven artifacts
-sbt publishM2
+#!/bin/bash
+
+set -e
+
+ONLY_MAVEN=0
+
+for arg in "$@"; do case $arg in
+  -o|--only-maven)
+    ONLY_MAVEN=1
+    shift
+    ;;
+  esac
+done
+
+if [ $ONLY_MAVEN == "0" ]; then
+  sbt publishM2
+fi
+
 SDK_VERSION=$(sbt "print sdk/version" | tail -1)
 cd maven-java
 mvn versions:set -DnewVersion=$SDK_VERSION
