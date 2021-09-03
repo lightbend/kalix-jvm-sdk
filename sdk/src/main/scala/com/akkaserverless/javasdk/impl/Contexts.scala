5498f97d15a110051b5411f4f2e451e1ffbe877b
Peter Vlugter <pvlugter@gmail.com>
2021-09-03 17:44:05 +1200

    Clean up some obsolete reflection and reply code

--- sdk/src/main/scala/com/akkaserverless/javasdk/impl/Contexts.scala
+++ sdk/src/main/scala/com/akkaserverless/javasdk/impl/Contexts.scala
@@ -16,66 +16,13 @@
 
 package com.akkaserverless.javasdk.impl
 
-import com.akkaserverless.javasdk
-import com.akkaserverless.javasdk.impl.reply.{NoReply, ReplySupport}
 import com.akkaserverless.javasdk.Context
-import com.akkaserverless.javasdk.reply._
-import com.akkaserverless.protocol.component._
-import com.google.protobuf.{Any => JavaPbAny}
 import scala.util.control.NoStackTrace
-
-import com.akkaserverless.javasdk.impl.effect.EffectSupport
-import com.akkaserverless.javasdk.impl.effect.SecondaryEffectImpl
 
 private[impl] trait ActivatableContext extends Context {
   private final var active = true
   final def deactivate(): Unit = active = false
   final def checkActive(): Unit = if (!active) throw new IllegalStateException("Context no longer active!")
-}
-
-// FIXME this is only used by ReplicatedEntity and should be implemented like in other entities.
-//   See SecondaryEffectImpl.replyToClientAction
-private[impl] trait AbstractClientActionContext {
-  self: ActivatableContext =>
-
-  def commandId: Long
-
-  private final var error: Option[String] = None
-  private final var forward: Option[Forward] = None
-
-  final def hasError: Boolean = error.isDefined
-
-  protected def logError(message: String): Unit = ()
-
-  final def replyToClientAction(msg: javasdk.Reply[JavaPbAny],
-                                allowNoReply: Boolean,
-                                restartOnFailure: Boolean): Option[ClientAction] =
-    error match {
-      case Some(msg) => Some(ClientAction(ClientAction.Action.Failure(Failure(commandId, msg, restartOnFailure))))
-      case None =>
-        msg match {
-          case message: MessageReply[JavaPbAny] =>
-            if (forward.isDefined) {
-              throw new IllegalStateException(
-                "Both a reply was returned, and a forward message was sent, choose one or the other."
-              )
-            }
-            Some(ClientAction(ClientAction.Action.Reply(ReplySupport.asProtocol(message))))
-          case forward: ForwardReply[JavaPbAny] =>
-            Some(ClientAction(ClientAction.Action.Forward(ReplySupport.asProtocol(forward))))
-          case failure: ErrorReply[JavaPbAny] =>
-            Some(ClientAction(ClientAction.Action.Failure(Failure(commandId, failure.description(), restartOnFailure))))
-          case _: NoReply[_] =>
-            if (forward.isDefined) {
-              Some(ClientAction(ClientAction.Action.Forward(forward.get)))
-            } else if (allowNoReply) {
-              None
-            } else {
-              throw new RuntimeException("No reply or forward returned by command handler!")
-            }
-        }
-    }
-
 }
 
 object FailInvoked extends RuntimeException with NoStackTrace {
