17416e9ff712b176dcf5ad60d7a6c7298099fad3
Peter Vlugter <pvlugter@gmail.com>
2021-09-09 19:13:55 +1200

    Add docs and examples for replicated data types

---
+++ samples/replicatedentity-examples/src/it/java/com/example/replicated/register/domain/SomeRegisterIntegrationTest.java
@@ -0,0 +1,52 @@
+package com.example.replicated.register.domain;
+
+import com.akkaserverless.javasdk.testkit.junit.AkkaServerlessTestkitResource;
+import com.example.replicated.Main;
+import com.example.replicated.register.RegisterServiceClient;
+import com.example.replicated.register.SomeRegisterApi;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static java.util.concurrent.TimeUnit.*;
+import static org.junit.Assert.assertEquals;
+
+// Run all test classes ending with "IntegrationTest" using `mvn verify -Pit`
+public class SomeRegisterIntegrationTest {
+
+  /** The test kit starts both the service container and the Akka Serverless proxy. */
+  @ClassRule
+  public static final AkkaServerlessTestkitResource testkit =
+      new AkkaServerlessTestkitResource(Main.createAkkaServerless());
+
+  /** Use the generated gRPC client to call the service through the Akka Serverless proxy. */
+  private final RegisterServiceClient client;
+
+  public SomeRegisterIntegrationTest() {
+    client =
+        RegisterServiceClient.create(testkit.getGrpcClientSettings(), testkit.getActorSystem());
+  }
+
+  public void set(String registerId, String value) throws Exception {
+    client
+        .set(
+            SomeRegisterApi.SetValue.newBuilder().setRegisterId(registerId).setValue(value).build())
+        .toCompletableFuture()
+        .get(5, SECONDS);
+  }
+
+  public SomeRegisterApi.CurrentValue get(String registerId) throws Exception {
+    return client
+        .get(SomeRegisterApi.GetValue.newBuilder().setRegisterId(registerId).build())
+        .toCompletableFuture()
+        .get(5, SECONDS);
+  }
+
+  @Test
+  public void interactWithSomeRegister() throws Exception {
+    assertEquals("", get("register1").getValue());
+    set("register1", "one");
+    assertEquals("one", get("register1").getValue());
+    set("register1", "two");
+    assertEquals("two", get("register1").getValue());
+  }
+}
