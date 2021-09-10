a655e84b39aaaf420a63a9b3a61ca1f207db2fc6
Johan Andrén <johan@markatta.com>
2021-09-10 10:43:43 +0200

    Avoid using the domain state type for the view in sample #317
    Also a short note in the docs for where we use the same state type.

--- samples/java-eventsourced-customer-registry/src/main/java/customer/view/CustomerByNameView.java
+++ samples/java-eventsourced-customer-registry/src/main/java/customer/view/CustomerByNameView.java
@@ -8,48 +8,69 @@ package customer.view;
 import com.akkaserverless.javasdk.view.ViewContext;
 import com.google.protobuf.Any;
 import customer.domain.CustomerDomain;
+import customer.api.CustomerApi;
 
 public class CustomerByNameView extends AbstractCustomerByNameView { // <1>
 
   public CustomerByNameView(ViewContext context) {}
 
   @Override
-  public CustomerDomain.CustomerState emptyState() { // <2>
+  public CustomerApi.Customer emptyState() { // <2>
     return null;
   }
 
   @Override // <3>
-  public UpdateEffect<CustomerDomain.CustomerState> processCustomerCreated(
-    CustomerDomain.CustomerState state,
+  public UpdateEffect<CustomerApi.Customer> processCustomerCreated(
+    CustomerApi.Customer state,
     CustomerDomain.CustomerCreated customerCreated) {
     if (state != null) {
       return effects().ignore(); // already created
     } else {
-      return effects().updateState(customerCreated.getCustomer());
+      return effects().updateState(convertToApi(customerCreated.getCustomer()));
     }
   }
 
   @Override // <3>
-  public UpdateEffect<CustomerDomain.CustomerState> processCustomerNameChanged(
-    CustomerDomain.CustomerState state,
+  public UpdateEffect<CustomerApi.Customer> processCustomerNameChanged(
+    CustomerApi.Customer state,
     CustomerDomain.CustomerNameChanged customerNameChanged) {
     return effects().updateState(
         state.toBuilder().setName(customerNameChanged.getNewName()).build());
   }
 
   @Override // <3>
-  public UpdateEffect<CustomerDomain.CustomerState> processCustomerAddressChanged(
-    CustomerDomain.CustomerState state,
+  public UpdateEffect<CustomerApi.Customer> processCustomerAddressChanged(
+    CustomerApi.Customer state,
     CustomerDomain.CustomerAddressChanged customerAddressChanged) {
     return effects().updateState(
-        state.toBuilder().setAddress(customerAddressChanged.getNewAddress()).build());
+        state.toBuilder().setAddress(convertToApi(customerAddressChanged.getNewAddress())).build());
   }
 
   @Override
-  public UpdateEffect<CustomerDomain.CustomerState> ignoreOtherEvents(
-    CustomerDomain.CustomerState state,
+  public UpdateEffect<CustomerApi.Customer> ignoreOtherEvents(
+    CustomerApi.Customer state,
     Any any) {
     return effects().ignore();
+  }
+
+  private CustomerApi.Customer convertToApi(CustomerDomain.CustomerState s) {
+    CustomerApi.Address address = CustomerApi.Address.getDefaultInstance();
+    if (s.hasAddress()) {
+      address = convertToApi(s.getAddress());
+    }
+    return CustomerApi.Customer.newBuilder()
+        .setCustomerId(s.getCustomerId())
+        .setEmail(s.getEmail())
+        .setName(s.getName())
+        .setAddress(address)
+        .build();
+  }
+
+  private CustomerApi.Address convertToApi(CustomerDomain.Address a) {
+    return CustomerApi.Address.newBuilder()
+        .setStreet(a.getStreet())
+        .setCity(a.getCity())
+        .build();
   }
 }
 // end::process-events[]
