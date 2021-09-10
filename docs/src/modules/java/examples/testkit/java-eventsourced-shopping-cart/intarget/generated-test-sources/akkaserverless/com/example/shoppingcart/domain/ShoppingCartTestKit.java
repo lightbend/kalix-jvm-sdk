65aaf6e066e6fbd5145ac52c9917e23faddd85ea
francisco <franciscolopezsancho@gmail.com>
2021-09-10 17:01:29 +0200

    rewrite of docs

--- docs/src/modules/java/examples/testkit/java-eventsourced-shopping-cart/intarget/generated-test-sources/akkaserverless/com/example/shoppingcart/domain/ShoppingCartTestKit.java
+++ docs/src/modules/java/examples/testkit/java-eventsourced-shopping-cart/intarget/generated-test-sources/akkaserverless/com/example/shoppingcart/domain/ShoppingCartTestKit.java
@@ -10,14 +10,34 @@ import java.util.List;
 public class ShoppingCartTestKit {
 
 
-  public ShoppingCartTestKit(ShoppingCart entity) // <1>
+  /**
+   * Create a testkit instance of ShoppingCart
+   * @param entityFactory A function that creates a ShoppingCart based on the given EventSourcedEntityContext,
+   *                      a default entity id is used.
+   */
+  public static ShoppingCartTestKit of(Function<EventSourcedEntityContext, ShoppingCart> entityFactory) // <1>
 
-  public ShoppingCartTestKit(ShoppingCart entity, ShoppingCartDomain.Cart state) // <2>
+  /**
+   * Create a testkit instance of ShoppingCart with a specific entity id.
+   */
+  public static ShoppingCartTestKit of(String entityId, Function<EventSourcedEntityContext, ShoppingCart> entityFactory) // <2>
 
+  /**
+   * @return The current state of the ShoppingCart under test
+   */
   public ShoppingCartDomain.Cart getState() // <3>
 
+  /**
+   * @return All events that has been emitted by command handlers since the creation of this testkit.
+   *         Individual sets of events from a single command handler invokation can be found in the
+   *         Result from calling it.
+   */
   public List<Object> getAllEvents() // <4>
 
-  public Result<Empty> addItem(ShoppingCartApi.AddLineItem command) // <5>
+  public EventSourcedResult<Empty> addItem(ShoppingCartApi.AddLineItem command) // <5>
+
+  public EventSourcedResult<Empty> removeItem(ShoppingCartApi.RemoveLineItem command) // <5>
+
+  public EventSourcedResult<ShoppingCartApi.Cart> getCart(ShoppingCartApi.GetShoppingCart command) // <5>
   
 }
