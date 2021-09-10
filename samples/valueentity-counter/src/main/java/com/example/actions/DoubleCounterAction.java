fae4926fa1dc1a1a3efc5de304511fd270cb02b1
Renato Cavalcanti <renato@cavalcanti.be>
2021-09-10 09:56:06 +0200

    document side effect with action controller (#333)

--- samples/valueentity-counter/src/main/java/com/example/actions/DoubleCounterAction.java
+++ samples/valueentity-counter/src/main/java/com/example/actions/DoubleCounterAction.java
@@ -6,20 +6,22 @@
 package com.example.actions;
 
 import com.akkaserverless.javasdk.ServiceCallRef;
+import com.akkaserverless.javasdk.SideEffect;
 import com.akkaserverless.javasdk.action.ActionCreationContext;
 import com.example.CounterApi;
 import com.google.protobuf.Empty;
 
-// tag::double-counter-action[]
+// tag::controller-forward[]
+// tag::controller-side-effect[]
 /**
  * An action.
  */
 public class DoubleCounterAction extends AbstractDoubleCounterAction {
 
-  private final ServiceCallRef<CounterApi.IncreaseValue> callRef;
+  private final ServiceCallRef<CounterApi.IncreaseValue> increaseCallRef;
 
   public DoubleCounterAction(ActionCreationContext creationContext) {
-    this.callRef =
+    this.increaseCallRef =
         creationContext.serviceCallFactory() // <1>
             .lookup(
                 "com.example.CounterService",
@@ -32,12 +34,29 @@ public class DoubleCounterAction extends AbstractDoubleCounterAction {
    */
   @Override
   public Effect<Empty> increase(CounterApi.IncreaseValue increaseValue) {
-
+  // end::controller-side-effect[]
     int doubled = increaseValue.getValue() * 2;
     CounterApi.IncreaseValue increaseValueDoubled =
         increaseValue.toBuilder().setValue(doubled).build(); // <2>
 
-    return effects().forward(callRef.createCall(increaseValueDoubled)); // <3>
+    return effects()
+            .forward(increaseCallRef.createCall(increaseValueDoubled)); // <3>
   }
+
+  // end::controller-forward[]
+  public Effect<Empty> increaseWithSideEffect(CounterApi.IncreaseValue increaseValue) {
+  // tag::controller-side-effect[]
+    int doubled = increaseValue.getValue() * 2;
+    CounterApi.IncreaseValue increaseValueDoubled =
+        increaseValue.toBuilder().setValue(doubled).build(); // <2>
+
+    return effects()
+            .reply(Empty.getDefaultInstance()) // <3>
+            .addSideEffect( // <4>
+                SideEffect.of(increaseCallRef.createCall(increaseValueDoubled)));
+  }
+  // tag::controller-forward[]
+
 }
-// end::double-counter-action[]
+// end::controller-forward[]
+// end::controller-side-effect[]
