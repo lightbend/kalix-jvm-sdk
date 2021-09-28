74a01faf5916d4b23a473bfd1f77eae991977a56
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-09-28 12:23:59 +0200

    docs: Scala in views docs
    * still rather many things left as TODO because
      they depend on the event sourced sample

--- samples/scala-valueentity-customer-registry/src/main/scala/customer/Main.scala
+++ samples/scala-valueentity-customer-registry/src/main/scala/customer/Main.scala
@@ -13,19 +13,30 @@ object Main {
 
   private val log = LoggerFactory.getLogger("customer.Main")
 
+  // tag::register[]
   def createAkkaServerless(): AkkaServerless = {
     // The AkkaServerlessFactory automatically registers any generated Actions, Views or Entities,
     // and is kept up-to-date with any changes in your protobuf definitions.
     // If you prefer, you may remove this and manually register these components in a
     // `AkkaServerless()` instance.
     AkkaServerlessFactory.withComponents(
+      // end::register[]
       new CustomerValueEntity(_),
       new CustomerActionImpl(_),
       new CustomerByEmailView(_),
       new CustomerByNameView(_),
       new CustomerSummaryByNameView(_),
       new CustomersResponseByNameView(_))
+      /*
+      // tag::register[]
+      return AkkaServerlessFactory.withComponents(
+        new CustomerValueEntity(_),
+        new CustomerByNameView(_))
+      // end::register[]
+      */
+    // tag::register[]
   }
+  // end::register[]
 
   def main(args: Array[String]): Unit = {
     log.info("starting the Akka Serverless service")
