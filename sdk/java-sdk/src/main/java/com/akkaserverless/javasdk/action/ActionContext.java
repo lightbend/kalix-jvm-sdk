742af2c96119f60aaa3fc229b1cfadae4862740f
francisco <franciscolopezsancho@gmail.com>
2021-10-04 19:39:41 +0100

    adding FIXME

--- sdk/java-sdk/src/main/java/com/akkaserverless/javasdk/action/ActionContext.java
+++ sdk/java-sdk/src/main/java/com/akkaserverless/javasdk/action/ActionContext.java
@@ -35,7 +35,7 @@ public interface ActionContext extends MetadataContext {
    *
    * @return The call level metadata.
    */
-  Metadata metadata(); // TODO this is not necessary
+  Metadata metadata(); // FIX: remove this method and move docs pointing to this method to MetadataContext
 
   /**
    * The origin subject of the {@link CloudEvent}. For example, the entity key when the event was
