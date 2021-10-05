957132996c3e509607d96002a3f2159980c4f2f0
Arnout Engelen <arnout@bzzt.net>
2021-10-05 17:27:59 +0000

    indent

--- codegen/scala-gen/src/test/scala/com/akkaserverless/codegen/scalasdk/ValueEntityTestKitGeneratorSuite.scala
+++ codegen/scala-gen/src/test/scala/com/akkaserverless/codegen/scalasdk/ValueEntityTestKitGeneratorSuite.scala
@@ -172,56 +172,56 @@ class ValueEntityTestKitGeneratorSuite extends munit.FunSuite {
     assertEquals(
       ValueEntityTestKitGenerator.integrationTest(main, entity, service).content,
       """package com.example.shoppingcart.api
-
-import com.akkaserverless.scalasdk.testkit.AkkaServerlessTestkit
-import com.akkaserverless.scalasdk.testkit.ValueEntityResult
-import com.akkaserverless.scalasdk.valueentity.ValueEntity
-import com.example.shoppingcart.Main
-import com.example.shoppingcart.domain
-import com.google.protobuf.Empty
-import org.scalatest.BeforeAndAfterAll
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.matchers.should.Matchers
-import org.scalatest.time.Millis
-import org.scalatest.time.Seconds
-import org.scalatest.time.Span
-import org.scalatest.wordspec.AnyWordSpec
-
-// This class was initially generated based on the .proto definition by Akka Serverless tooling.
-//
-// As long as this file exists it will not be overwritten: you can maintain it yourself,
-// or delete it so it is regenerated as needed.
-
-class ShoppingCartServiceIntegrationSpec
-    extends AnyWordSpec
-    with Matchers
-    with BeforeAndAfterAll
-    with ScalaFutures {
-
-  implicit val patience: PatienceConfig =
-    PatienceConfig(Span(5, Seconds), Span(500, Millis))
-
-  val testkit = AkkaServerlessTestkit(Main.createAkkaServerless())
-  testkit.start()
-  implicit val system = testkit.system
-
-  "ShoppingCartService" must {
-    val client: ShoppingCartServiceClient =
-      ShoppingCartServiceClient(testkit.grpcClientSettings)
-
-    "have example test that can be removed" in {
-      // use the gRPC client to send requests to the
-      // proxy and verify the results
-    }
-
-  }
-
-  override def afterAll() = {
-    testkit.stop()
-    super.afterAll()
-  }
-}
-""".stripMargin)
+        |
+        |import com.akkaserverless.scalasdk.testkit.AkkaServerlessTestkit
+        |import com.akkaserverless.scalasdk.testkit.ValueEntityResult
+        |import com.akkaserverless.scalasdk.valueentity.ValueEntity
+        |import com.example.shoppingcart.Main
+        |import com.example.shoppingcart.domain
+        |import com.google.protobuf.Empty
+        |import org.scalatest.BeforeAndAfterAll
+        |import org.scalatest.concurrent.ScalaFutures
+        |import org.scalatest.matchers.should.Matchers
+        |import org.scalatest.time.Millis
+        |import org.scalatest.time.Seconds
+        |import org.scalatest.time.Span
+        |import org.scalatest.wordspec.AnyWordSpec
+        |
+        |// This class was initially generated based on the .proto definition by Akka Serverless tooling.
+        |//
+        |// As long as this file exists it will not be overwritten: you can maintain it yourself,
+        |// or delete it so it is regenerated as needed.
+        |
+        |class ShoppingCartServiceIntegrationSpec
+        |    extends AnyWordSpec
+        |    with Matchers
+        |    with BeforeAndAfterAll
+        |    with ScalaFutures {
+        |
+        |  implicit val patience: PatienceConfig =
+        |    PatienceConfig(Span(5, Seconds), Span(500, Millis))
+        |
+        |  val testkit = AkkaServerlessTestkit(Main.createAkkaServerless())
+        |  testkit.start()
+        |  implicit val system = testkit.system
+        |
+        |  "ShoppingCartService" must {
+        |    val client: ShoppingCartServiceClient =
+        |      ShoppingCartServiceClient(testkit.grpcClientSettings)
+        |
+        |    "have example test that can be removed" in {
+        |      // use the gRPC client to send requests to the
+        |      // proxy and verify the results
+        |    }
+        |
+        |  }
+        |
+        |  override def afterAll() = {
+        |    testkit.stop()
+        |    super.afterAll()
+        |  }
+        |}
+        |""".stripMargin)
   }
 
   def createShoppingCartEntity(): ModelBuilder.ValueEntity = {
