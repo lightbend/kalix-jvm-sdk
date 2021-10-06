7e442cb65e1df1b66b84e0516e48ed0635d89b9d
Renato Cavalcanti <renato@cavalcanti.be>
2021-10-06 17:14:48 +0200

    java formatting

--- sdk/core/src/main/java/com/akkaserverless/replicatedentity/ReplicatedData.java
+++ sdk/core/src/main/java/com/akkaserverless/replicatedentity/ReplicatedData.java
@@ -22,10 +22,10 @@ import akka.annotation.InternalApi;
 public interface ReplicatedData {
 
   /**
-   * INTERNAL API
-   * This method is used internally to control if instances have been created by internal factory methods.
-   * Specially needed for the Scala SDK that wraps the Java implementation. In which case, _internal() will return
-   * the Java internal delegate. The Java implementations will return {@code this}.
+   * INTERNAL API This method is used internally to control if instances have been created by
+   * internal factory methods. Specially needed for the Scala SDK that wraps the Java
+   * implementation. In which case, _internal() will return the Java internal delegate. The Java
+   * implementations will return {@code this}.
    */
   @InternalApi
   ReplicatedData _internal();
