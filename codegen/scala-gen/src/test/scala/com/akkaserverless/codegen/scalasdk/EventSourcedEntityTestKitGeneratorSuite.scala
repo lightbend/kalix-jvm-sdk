d962a7921ae9619c02441d47941ff4ebb95f761a
Arnout Engelen <arnout@bzzt.net>
2021-10-08 15:08:21 +0000

    Add EventSourcedEntity IntegrationSpec template
    Part of #372

--- codegen/scala-gen/src/test/scala/com/akkaserverless/codegen/scalasdk/EventSourcedEntityTestKitGeneratorSuite.scala
+++ codegen/scala-gen/src/test/scala/com/akkaserverless/codegen/scalasdk/EventSourcedEntityTestKitGeneratorSuite.scala
@@ -130,4 +130,64 @@ class EventSourcedEntityTestKitGeneratorSuite extends munit.FunSuite {
          |""".stripMargin)
   }
 
+  test("it can generate an specific integration test stub for the entity") {
+    val main = FullyQualifiedName("Main", packageNaming.copy(protoPackage = "com.example"))
+
+    assertEquals(
+      EventSourcedEntityTestKitGenerator
+        .integrationTest(main, testData.eventSourcedEntity(), testData.simpleEntityService())
+        .content,
+      s"""package com.example.service
+         |
+         |import akka.actor.ActorSystem
+         |import com.akkaserverless.scalasdk.eventsourcedentity.EventSourcedEntity
+         |import com.akkaserverless.scalasdk.testkit.AkkaServerlessTestKit
+         |import com.akkaserverless.scalasdk.testkit.EventSourcedResult
+         |import com.example.Main
+         |import com.external.Empty
+         |import org.scalatest.BeforeAndAfterAll
+         |import org.scalatest.concurrent.ScalaFutures
+         |import org.scalatest.matchers.should.Matchers
+         |import org.scalatest.time.Millis
+         |import org.scalatest.time.Seconds
+         |import org.scalatest.time.Span
+         |import org.scalatest.wordspec.AnyWordSpec
+         |
+         |// This class was initially generated based on the .proto definition by Akka Serverless tooling.
+         |//
+         |// As long as this file exists it will not be overwritten: you can maintain it yourself,
+         |// or delete it so it is regenerated as needed.
+         |
+         |class MyServiceIntegrationSpec
+         |    extends AnyWordSpec
+         |    with Matchers
+         |    with BeforeAndAfterAll
+         |    with ScalaFutures {
+         |
+         |  implicit val patience: PatienceConfig =
+         |    PatienceConfig(Span(5, Seconds), Span(500, Millis))
+         |
+         |  val testKit = AkkaServerlessTestKit(Main.createAkkaServerless())
+         |  testKit.start()
+         |  implicit val system: ActorSystem = testKit.system
+         |
+         |  "MyService" must {
+         |    val client: MyServiceClient =
+         |      MyServiceClient(testKit.grpcClientSettings)
+         |
+         |    "have example test that can be removed" in {
+         |      // use the gRPC client to send requests to the
+         |      // proxy and verify the results
+         |    }
+         |
+         |  }
+         |
+         |  override def afterAll() = {
+         |    testKit.stop()
+         |    super.afterAll()
+         |  }
+         |}
+         |""".stripMargin)
+  }
+
 }
