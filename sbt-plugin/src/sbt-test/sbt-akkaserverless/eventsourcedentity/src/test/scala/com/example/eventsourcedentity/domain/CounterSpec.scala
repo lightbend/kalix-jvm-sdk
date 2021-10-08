17c9bf6e2f9c0dd407ece73a107c9994e5b7e606
Arnout Engelen <arnout@bzzt.net>
2021-10-08 08:18:11 +0000

    Add scripted test

---
+++ sbt-plugin/src/sbt-test/sbt-akkaserverless/eventsourcedentity/src/test/scala/com/example/eventsourcedentity/domain/CounterSpec.scala
@@ -0,0 +1,57 @@
+package com.example.eventsourcedentity.domain
+
+import com.akkaserverless.scalasdk.eventsourcedentity.EventSourcedEntity
+import com.akkaserverless.scalasdk.testkit.EventSourcedResult
+import com.example.eventsourcedentity
+import com.google.protobuf.empty.Empty
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+// This class was initially generated based on the .proto definition by Akka Serverless tooling.
+//
+// As long as this file exists it will not be overwritten: you can maintain it yourself,
+// or delete it so it is regenerated as needed.
+
+class CounterSpec extends AnyWordSpec with Matchers {
+  val counterId = "37"
+
+  "The Counter" should {
+    "correctly process commands of type Increase" in {
+      val testKit = CounterTestKit(new Counter(_))
+
+      testKit.increase(eventsourcedentity.IncreaseValue(counterId, 42))
+      val current = testKit.getCurrentCounter(eventsourcedentity.GetCounter(counterId))
+      current.reply.value shouldBe(42)
+
+      testKit.increase(eventsourcedentity.IncreaseValue(counterId, 15))
+      val next = testKit.getCurrentCounter(eventsourcedentity.GetCounter(counterId))
+      next.reply.value shouldBe(42+15)
+    }
+
+    "correctly process commands of type Decrease" in {
+      val testKit = CounterTestKit(new Counter(_))
+
+      testKit.increase(eventsourcedentity.IncreaseValue(counterId, 42))
+      val current = testKit.getCurrentCounter(eventsourcedentity.GetCounter(counterId))
+      current.reply.value shouldBe(42)
+
+      testKit.decrease(eventsourcedentity.DecreaseValue(counterId, 15))
+      val next = testKit.getCurrentCounter(eventsourcedentity.GetCounter(counterId))
+      next.reply.value shouldBe(42-15)
+    }
+
+    "correctly process commands of type Reset" in {
+      val testKit = CounterTestKit(new Counter(_))
+
+      testKit.increase(eventsourcedentity.IncreaseValue(counterId, 42))
+      val current = testKit.getCurrentCounter(eventsourcedentity.GetCounter(counterId))
+      current.reply.value shouldBe(42)
+
+      val reset = testKit.reset(eventsourcedentity.ResetValue(counterId))
+      reset.nextEventOfType(classOf[Decreased]).value shouldBe(42)
+
+      val next = testKit.getCurrentCounter(eventsourcedentity.GetCounter(counterId))
+      next.reply.value shouldBe(0)
+    }
+  }
+}
