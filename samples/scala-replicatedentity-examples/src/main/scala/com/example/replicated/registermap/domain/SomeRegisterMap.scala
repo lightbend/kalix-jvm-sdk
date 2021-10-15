223fe003040db72e3bea3e8ffdf3570788aed29c
Johan Andrén <johan@markatta.com>
2021-10-15 15:08:05 +0200

    docs(scala): All replicated data types documented

--- samples/scala-replicatedentity-examples/src/main/scala/com/example/replicated/registermap/domain/SomeRegisterMap.scala
+++ samples/scala-replicatedentity-examples/src/main/scala/com/example/replicated/registermap/domain/SomeRegisterMap.scala
@@ -14,60 +14,41 @@ import com.google.protobuf.empty.Empty
 /** A replicated entity. */
 class SomeRegisterMap(context: ReplicatedEntityContext) extends AbstractSomeRegisterMap {
 
-  /** Command handler for "Set". */
+  // tag::update[]
   def set(currentData: ReplicatedRegisterMap[SomeKey, SomeValue], setValue: registermap.SetValue): ReplicatedEntity.Effect[Empty] = {
-    
-    val keyValue = 
-      for {
-        key <- setValue.key.map(_.field)
-        value <- setValue.value.map(_.field)
-      } yield (key, value)
-    
-      keyValue.map { case (key, value) => 
-        effects
-          .update(currentData.setValue(SomeKey(key), SomeValue(value)))
-          .thenReply(Empty.defaultInstance)
-      }
-      .getOrElse {
-        effects
-          .error(s"Invalid input: received key = '${setValue.key}' and value = '${setValue.value}'")
-      }
-    
+    val key = SomeKey(setValue.getKey.field) // <1>
+    val value = SomeValue(setValue.getValue.field) // <2>
+    effects
+      .update(currentData.setValue(key,value)) // <3>
+      .thenReply(Empty.defaultInstance)
   }
 
-  /** Command handler for "Remove". */
-  def remove(currentData: ReplicatedRegisterMap[SomeKey, SomeValue], removeValue: registermap.RemoveValue): ReplicatedEntity.Effect[Empty] = 
-    removeValue.key.map { key => 
-      effects
-        .update(currentData.remove(SomeKey(key.field)))
-        .thenReply(Empty.defaultInstance)
-    }
-    .getOrElse { 
-      effects.error("Invalid input: key is empty")
-    }
-  
+  def remove(currentData: ReplicatedRegisterMap[SomeKey, SomeValue], removeValue: registermap.RemoveValue): ReplicatedEntity.Effect[Empty] = {
+    val key = SomeKey(removeValue.getKey.field) // <1>
+    effects
+      .update(currentData.remove(key)) // <3>
+      .thenReply(Empty.defaultInstance)
+  }
+  // end::update[]
 
-  /** Command handler for "Get". */
-  def get(currentData: ReplicatedRegisterMap[SomeKey, SomeValue], getValue: registermap.GetValue): ReplicatedEntity.Effect[registermap.CurrentValue] = 
-    getValue.key.map { key => 
-      val value = currentData.get(SomeKey(key.field)).map(v => registermap.Value(v.someField))
-      effects
-        .reply(registermap.CurrentValue(getValue.key, value))
-    }
-    .getOrElse {
-      effects.error("The command handler for `Get` is not implemented, yet")
-    }
+  // tag::get[]
+  def get(currentData: ReplicatedRegisterMap[SomeKey, SomeValue], getValue: registermap.GetValue): ReplicatedEntity.Effect[registermap.CurrentValue] = {
+    val key = SomeKey(getValue.getKey.field) // <1>
+    val maybeValue = currentData.get(key) // <2>
+    val currentValue = registermap.CurrentValue(getValue.key, maybeValue.map(v => registermap.Value(v.someField)))
+    effects.reply(currentValue)
+  }
 
-  /** Command handler for "GetAll". */
   def getAll(currentData: ReplicatedRegisterMap[SomeKey, SomeValue], getAllValues: registermap.GetAllValues): ReplicatedEntity.Effect[registermap.CurrentValues] = {
 
     val allData = 
-      currentData.keySet.map { key => 
+      currentData.keySet.map { key => // <3>
         val value = currentData.get(key).map(v => registermap.Value(v.someField))
         registermap.CurrentValue(Some(registermap.Key(key.someField)), value)
       }.toSeq
 
     effects.reply(registermap.CurrentValues(allData))
   }
+  // end::get[]
 
 }
