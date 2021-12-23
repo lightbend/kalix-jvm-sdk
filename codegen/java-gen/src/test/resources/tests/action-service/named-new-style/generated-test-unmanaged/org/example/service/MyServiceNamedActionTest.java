c2d7be3331f79f4f2a42f58f4c5475982b263620
Renato Cavalcanti <renato@cavalcanti.be>
2021-12-23 13:00:01 +0100

    chore: ExampleSuite tests for named action (new codegen) (#777)

---
+++ codegen/java-gen/src/test/resources/tests/action-service/named-new-style/generated-test-unmanaged/org/example/service/MyServiceNamedActionTest.java
@@ -0,0 +1,54 @@
+package org.example.service;
+
+import akka.NotUsed;
+import akka.stream.javadsl.Source;
+import com.akkaserverless.javasdk.testkit.ActionResult;
+import com.google.protobuf.Empty;
+import org.example.service.MyServiceNamedAction;
+import org.example.service.MyServiceNamedActionTestKit;
+import org.example.service.ServiceOuterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+// This class was initially generated based on the .proto definition by Akka Serverless tooling.
+//
+// As long as this file exists it will not be overwritten: you can maintain it yourself,
+// or delete it so it is regenerated as needed.
+
+public class MyServiceNamedActionTest {
+
+  @Test
+  public void exampleTest() {
+    MyServiceNamedActionTestKit testKit = MyServiceNamedActionTestKit.of(MyServiceNamedAction::new);
+    // use the testkit to execute a command
+    // ActionResult<SomeResponse> result = testKit.someOperation(SomeRequest);
+    // verify the response
+    // SomeResponse actualResponse = result.getReply();
+    // assertEquals(expectedResponse, actualResponse);
+  }
+
+  @Test
+  public void simpleMethodTest() {
+    MyServiceNamedActionTestKit testKit = MyServiceNamedActionTestKit.of(MyServiceNamedAction::new);
+    // ActionResult<Empty> result = testKit.simpleMethod(ServiceOuterClass.MyRequest.newBuilder()...build());
+  }
+
+  @Test
+  public void streamedOutputMethodTest() {
+    MyServiceNamedActionTestKit testKit = MyServiceNamedActionTestKit.of(MyServiceNamedAction::new);
+    // Source<ActionResult<Empty>, akka.NotUsed> result = testKit.streamedOutputMethod(ServiceOuterClass.MyRequest.newBuilder()...build());
+  }
+
+  @Test
+  public void streamedInputMethodTest() {
+    MyServiceNamedActionTestKit testKit = MyServiceNamedActionTestKit.of(MyServiceNamedAction::new);
+    // ActionResult<Empty> result = testKit.streamedInputMethod(Source.single(ServiceOuterClass.MyRequest.newBuilder()...build()));
+  }
+
+  @Test
+  public void fullStreamedMethodTest() {
+    MyServiceNamedActionTestKit testKit = MyServiceNamedActionTestKit.of(MyServiceNamedAction::new);
+    // Source<ActionResult<Empty>, akka.NotUsed> result = testKit.fullStreamedMethod(Source.single(ServiceOuterClass.MyRequest.newBuilder()...build()));
+  }
+
+}
