a49e1e5b5fee3bc967b9778ad5a5c8015e0bf0c9
Renato Cavalcanti <renato@cavalcanti.be>
2022-01-26 22:44:07 +0100

    minor formatting

--- samples/java-eventsourced-counter-pojo/src/main/java/com/example/domain/CounterState.java
+++ samples/java-eventsourced-counter-pojo/src/main/java/com/example/domain/CounterState.java
@@ -11,12 +11,12 @@ public class CounterState {
     this.value = value;
   }
 
-  public CounterState increase ( int value){
+  public CounterState increase(int value){
     if (value <= 0) throw new IllegalArgumentException("increase value must be a positive number");
     return new CounterState(this.value + value);
   }
 
-  public CounterState decrease ( int value){
+  public CounterState decrease(int value){
     if (value >= 0) throw new IllegalArgumentException("decrease value must be a negative number");
     return new CounterState(this.value + value);
   }
