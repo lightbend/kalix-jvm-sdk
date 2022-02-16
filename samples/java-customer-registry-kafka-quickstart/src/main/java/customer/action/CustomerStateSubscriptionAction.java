729b6d67d27be75434ae8436eebcad5e5015c03c
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2022-02-16 14:29:24 +0000

    Adding kafka example (#834)
    * adding  example and docs

---
+++ samples/java-customer-registry-kafka-quickstart/src/main/java/customer/action/CustomerStateSubscriptionAction.java
@@ -0,0 +1,36 @@
+package customer.action;
+
+import com.akkaserverless.javasdk.action.ActionCreationContext;
+import customer.api.CustomerApi;
+import customer.domain.CustomerDomain;
+
+// This class was initially generated based on the .proto definition by Akka Serverless tooling.
+//
+// As long as this file exists it will not be overwritten: you can maintain it yourself,
+// or delete it so it is regenerated as needed.
+
+public class CustomerStateSubscriptionAction extends AbstractCustomerStateSubscriptionAction {
+
+  public CustomerStateSubscriptionAction(ActionCreationContext creationContext) {}
+// tag::upsert[]
+
+  @Override
+  public Effect<CustomerApi.Customer> onStateChange(CustomerDomain.CustomerState customerState) {
+   CustomerApi.Address address = CustomerApi.Address.newBuilder()
+            .setStreet(customerState.getAddress().getStreet())
+            .setCity(customerState.getAddress().getCity())
+            .build();
+
+   CustomerApi.Customer customer = CustomerApi.Customer.newBuilder()
+            .setCustomerId(customerState.getCustomerId())
+            .setEmail(customerState.getEmail())
+            .setName(customerState.getName())
+            .setAddress(address)
+            .build();
+
+
+    return effects().reply(customer);
+  }
+// end::upsert[]
+
+}
---
+++ samples/java-customer-registry-kafka-quickstart/src/test/java/customer/action/CustomerStateSubscriptionActionTest.java
@@ -0,0 +1,34 @@
+package customer.action;
+
+import akka.stream.javadsl.Source;
+import com.akkaserverless.javasdk.testkit.ActionResult;
+import customer.action.CustomerStateSubscriptionAction;
+import customer.action.CustomerStateSubscriptionActionTestKit;
+import customer.domain.CustomerDomain;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+// This class was initially generated based on the .proto definition by Akka Serverless tooling.
+//
+// As long as this file exists it will not be overwritten: you can maintain it yourself,
+// or delete it so it is regenerated as needed.
+
+public class CustomerStateSubscriptionActionTest {
+
+  @Test
+  public void exampleTest() {
+    CustomerStateSubscriptionActionTestKit testKit = CustomerStateSubscriptionActionTestKit.of(CustomerStateSubscriptionAction::new);
+    // use the testkit to execute a command
+    // ActionResult<SomeResponse> result = testKit.someOperation(SomeRequest);
+    // verify the response
+    // SomeResponse actualResponse = result.getReply();
+    // assertEquals(expectedResponse, actualResponse);
+  }
+
+  @Test
+  public void onStateChangeTest() {
+    CustomerStateSubscriptionActionTestKit testKit = CustomerStateSubscriptionActionTestKit.of(CustomerStateSubscriptionAction::new);
+    // ActionResult<CustomerDomain.CustomerState> result = testKit.onUpsertState(CustomerDomain.CustomerState.newBuilder()...build());
+  }
+
+}
