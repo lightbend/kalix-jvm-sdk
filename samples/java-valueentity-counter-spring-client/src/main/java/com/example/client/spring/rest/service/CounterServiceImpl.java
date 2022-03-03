226bad64933753403ccf9d250a86392d70e4f979
Brent Eritou <beritou@gmail.com>
2022-03-03 12:12:36 -0500

    Edit Spring client documentation
    Break out into sections Spring developers will expect:
    - What You Will Build
    - What You Need
    - How to get started
    - Create things
    - Build and Run Locally

--- samples/java-valueentity-counter-spring-client/src/main/java/com/example/client/spring/rest/service/CounterServiceImpl.java
+++ samples/java-valueentity-counter-spring-client/src/main/java/com/example/client/spring/rest/service/CounterServiceImpl.java
@@ -8,11 +8,10 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-
+// tag::getCurrentCounterCall[]
 @Service
 public class CounterServiceImpl implements CounterService {
 
-  // tag::getCurrentCounterCall[]
   @Autowired
   WebClient webClient;
 
