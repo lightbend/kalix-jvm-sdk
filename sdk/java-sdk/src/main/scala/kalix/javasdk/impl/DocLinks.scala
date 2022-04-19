e9fe40ec4683ff06714a481ddb4c4e4a04e8e3ad
Renato Cavalcanti <renato@cavalcanti.be>
2022-04-19 10:57:14 +0200

    chore: fix error prefix and add JWT

--- sdk/java-sdk/src/main/scala/kalix/javasdk/impl/DocLinks.scala
+++ sdk/java-sdk/src/main/scala/kalix/javasdk/impl/DocLinks.scala
@@ -21,22 +21,23 @@ object DocLinks {
   private val baseUrl = "https://docs.kalix.io"
 
   private val errorCodes = Map(
-    "AS-00112" -> s"$baseUrl/java/views.html#changing",
-    "AS-00402" -> s"$baseUrl/java/topic-eventing.html",
-    "AS-00406" -> s"$baseUrl/java/topic-eventing.html",
-    "AS-00414" -> s"$baseUrl/java/entity-eventing.html"
+    "KLX-00112" -> s"$baseUrl/java/views.html#changing",
+    "KLX-00402" -> s"$baseUrl/java/topic-eventing.html",
+    "KLX-00406" -> s"$baseUrl/java/topic-eventing.html",
+    "KLX-00414" -> s"$baseUrl/java/entity-eventing.html"
     // TODO: docs for value entity eventing (https://github.com/lightbend/kalix-jvm-sdk/issues/121)
-    // "AS-00415" -> s"$baseUrl/java/entity-eventing.html"
+    // "KLX-00415" -> s"$baseUrl/java/entity-eventing.html"
   )
 
   // fallback if not defined in errorCodes
   private val errorCodeCategories = Map(
-    "AS-001" -> s"$baseUrl/java/views.html",
-    "AS-002" -> s"$baseUrl/java/value-entity.html",
-    "AS-003" -> s"$baseUrl/java/eventsourced.html",
-    "AS-004" -> s"$baseUrl/java/", // no single page for eventing
-    "AS-005" -> s"$baseUrl/java/", // no docs yet for replicated entities
-    "AS-006" -> s"$baseUrl/java/proto.html#_transcoding_http" // all HTTP API errors
+    "KLX-001" -> s"$baseUrl/java/views.html",
+    "KLX-002" -> s"$baseUrl/java/value-entity.html",
+    "KLX-003" -> s"$baseUrl/java/eventsourced.html",
+    "KLX-004" -> s"$baseUrl/java/", // no single page for eventing
+    "KLX-005" -> s"$baseUrl/java/", // no docs yet for replicated entities
+    "KLX-006" -> s"$baseUrl/java/proto.html#_transcoding_http", // all HTTP API errors
+    "KLX-007" -> s"$baseUrl/services/using-jwts.html" 
   )
 
   def forErrorCode(code: String): Option[String] =
