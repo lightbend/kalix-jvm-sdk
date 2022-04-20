4fe11df50de9c775661612e3a4c9e536dca0451d
Renato Cavalcanti <renato@cavalcanti.be>
2022-04-20 11:28:41 +0200

    formatting

--- sdk/java-sdk/src/main/scala/kalix/javasdk/impl/DocLinks.scala
+++ sdk/java-sdk/src/main/scala/kalix/javasdk/impl/DocLinks.scala
@@ -37,8 +37,7 @@ object DocLinks {
     "KLX-004" -> s"$baseUrl/java/", // no single page for eventing
     "KLX-005" -> s"$baseUrl/java/", // no docs yet for replicated entities
     "KLX-006" -> s"$baseUrl/java/proto.html#_transcoding_http", // all HTTP API errors
-    "KLX-007" -> s"$baseUrl/services/using-jwts.html" 
-  )
+    "KLX-007" -> s"$baseUrl/services/using-jwts.html")
 
   def forErrorCode(code: String): Option[String] =
     errorCodes.get(code) match {
