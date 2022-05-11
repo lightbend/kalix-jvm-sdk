26d234a19bcb5ac2e82236a65bd4631864e1086f
Renato Cavalcanti <renato@cavalcanti.be>
2022-05-11 15:14:05 +0200

    remove obsolete ExecutionContext.Implicits.global

--- samples/scala-reliable-timers/src/main/scala/com/example/actions/OrderAction.scala
+++ samples/scala-reliable-timers/src/main/scala/com/example/actions/OrderAction.scala
@@ -7,7 +7,6 @@ import io.grpc.Status
 import io.grpc.StatusRuntimeException
 import org.slf4j.LoggerFactory
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
 
