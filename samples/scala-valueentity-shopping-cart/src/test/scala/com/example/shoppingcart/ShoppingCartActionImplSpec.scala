8ffbc713f0afd5228383233860eed14725d9b4b6
Eduardo Pinto <eduardo.pinto@lightbend.com>
2022-06-01 15:32:14 +0100

    Adding unit tests with cross-component dependencies to shopping cart

--- samples/scala-valueentity-shopping-cart/src/test/scala/com/example/shoppingcart/ShoppingCartActionImplSpec.scala
+++ samples/scala-valueentity-shopping-cart/src/test/scala/com/example/shoppingcart/ShoppingCartActionImplSpec.scala
@@ -1,9 +1,12 @@
 package com.example.shoppingcart
 
-import kalix.scalasdk.action.Action
-import kalix.scalasdk.testkit.ActionResult
+import com.google.protobuf.empty.Empty
+import kalix.scalasdk.testkit.impl.TestKitMockRegistry
+import org.scalamock.scalatest.{AsyncMockFactory}
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.wordspec.{AsyncWordSpec}
+
+import scala.concurrent.Future
 
 // This class was initially generated based on the .proto definition by Kalix tooling.
 //
@@ -11,13 +14,48 @@ import org.scalatest.wordspec.AnyWordSpec
 // or delete it so it is regenerated as needed.
 
 class ShoppingCartActionImplSpec
-    extends AnyWordSpec
-    with Matchers {
+    extends AsyncWordSpec
+    with Matchers
+    with AsyncMockFactory {
 
   "ShoppingCartActionImpl" must {
 
-    // Cannot be tested with unit testkit as it involves other components,
-    // needs an integration test for testing.
+    "initialize cart" in {
+      val mockShoppingCart = mock[ShoppingCartService]
+      (mockShoppingCart.create _)
+        .expects(*)
+        .returning(Future.successful(Empty.defaultInstance))
+      val mockRegistry = new TestKitMockRegistry(Map(classOf[ShoppingCartService] -> mockShoppingCart))
 
+      val service = ShoppingCartActionImplTestKit(new ShoppingCartActionImpl(_), mockRegistry)
+      val cartId = service.initializeCart(NewCart.defaultInstance).asyncResult
+
+      cartId.map { newCart => assert(newCart.reply.cartId.nonEmpty) }
+    }
+
+    "create a prepopulated cart" in {
+      val mockShoppingCart = stub[ShoppingCartService]
+      (mockShoppingCart.create _)
+        .when(*)
+        .returns(Future.successful(Empty.defaultInstance))
+      (mockShoppingCart.addItem _)
+        .when(where { li: AddLineItem => li.name == "eggplant"})
+        .returns(Future.successful(Empty.defaultInstance))
+
+      val mockRegistry = new TestKitMockRegistry(Map(classOf[ShoppingCartService] -> mockShoppingCart))
+
+      val service = ShoppingCartActionImplTestKit(new ShoppingCartActionImpl(_), mockRegistry)
+      val cartId = service.createPrePopulated(NewCart.defaultInstance).asyncResult
+
+      cartId.map { newCart =>
+        (mockShoppingCart.create _)
+          .verify(where { c: CreateCart => c.cartId == newCart.reply.cartId })
+          .once()
+        (mockShoppingCart.addItem _)
+          .verify(where { li: AddLineItem => li.cartId == newCart.reply.cartId })
+          .once()
+        succeed
+      }
+    }
   }
 }
