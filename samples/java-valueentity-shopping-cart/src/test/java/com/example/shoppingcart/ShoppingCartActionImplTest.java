ba8e0b3eecce22e0f1a61184f15ecb6295676f6d
Eduardo Pinto <eduardo.pinto@lightbend.com>
2022-06-02 11:24:13 +0100

    Add unit tests to java shopping cart sample

--- samples/java-valueentity-shopping-cart/src/test/java/com/example/shoppingcart/ShoppingCartActionImplTest.java
+++ samples/java-valueentity-shopping-cart/src/test/java/com/example/shoppingcart/ShoppingCartActionImplTest.java
@@ -1,29 +1,26 @@
 package com.example.shoppingcart;
 
-import akka.stream.javadsl.Source;
-import com.example.shoppingcart.domain.ShoppingCart;
 import com.google.protobuf.Empty;
 import kalix.javasdk.testkit.ActionResult;
-import com.example.shoppingcart.ShoppingCartActionImpl;
-import com.example.shoppingcart.ShoppingCartActionImplTestKit;
-import com.example.shoppingcart.ShoppingCartController;
 import kalix.javasdk.testkit.impl.TestKitMockRegistry;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.notNull;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import scala.collection.JavaConverters.AsScala;
 
 public class ShoppingCartActionImplTest {
 
@@ -43,18 +40,35 @@ public class ShoppingCartActionImplTest {
   }
 
   @Test
-  public void initializeCartTest() {
+  public void initializeCartTest() throws ExecutionException, InterruptedException, TimeoutException {
     when(shoppingCartService.create(notNull()))
-            .thenReturn(new CompletableFuture<>());
+            .thenReturn(CompletableFuture.completedFuture(Empty.getDefaultInstance()));
 
     TestKitMockRegistry mockRegistry = new TestKitMockRegistry(Map.of(ShoppingCartService.class, shoppingCartService));
-    // TODO inject mock registry
 
-    ShoppingCartActionImplTestKit testKit = ShoppingCartActionImplTestKit.of(ShoppingCartActionImpl::new);
+    ShoppingCartActionImplTestKit testKit = ShoppingCartActionImplTestKit.of(ShoppingCartActionImpl::new, mockRegistry);
     ActionResult<ShoppingCartController.NewCartCreated> result = testKit.initializeCart(ShoppingCartController.NewCart.newBuilder().build());
 
+    CompletableFuture<ActionResult<ShoppingCartController.NewCartCreated>> asyncResult = (CompletableFuture<ActionResult<ShoppingCartController.NewCartCreated>>) result.getAsyncResult();
+    assertFalse(asyncResult.get(1, TimeUnit.SECONDS).getReply().getCartId().isEmpty());
   }
 
+  @Test
+  public void prePopulatedCartTest() throws ExecutionException, InterruptedException, TimeoutException {
+    when(shoppingCartService.create(notNull()))
+            .thenReturn(CompletableFuture.completedFuture(Empty.getDefaultInstance()));
+    when(shoppingCartService.addItem(any()))
+            .thenReturn(CompletableFuture.completedFuture(Empty.getDefaultInstance()));
+    TestKitMockRegistry mockRegistry = new TestKitMockRegistry(Map.of(ShoppingCartService.class, shoppingCartService));
 
+    ShoppingCartActionImplTestKit testKit = ShoppingCartActionImplTestKit.of(ShoppingCartActionImpl::new, mockRegistry);
+    ActionResult<ShoppingCartController.NewCartCreated> result = testKit.createPrePopulated(ShoppingCartController.NewCart.newBuilder().build());
+    ShoppingCartController.NewCartCreated reply = ((CompletableFuture<ActionResult<ShoppingCartController.NewCartCreated>>) result.getAsyncResult()).get(1, TimeUnit.SECONDS).getReply();
+
+    ArgumentCaptor<ShoppingCartApi.AddLineItem> lineItem = ArgumentCaptor.forClass(ShoppingCartApi.AddLineItem.class);
+    verify(shoppingCartService).addItem(lineItem.capture());
+    assertEquals("eggplant", lineItem.getValue().getName());
+    assertEquals(reply.getCartId(), lineItem.getValue().getCartId());
+  }
 
 }
