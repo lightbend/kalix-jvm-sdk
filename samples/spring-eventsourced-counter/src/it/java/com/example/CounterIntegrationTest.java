d17548750632ad413183238c23005d2eef4182e0
Eduardo Pinto <eduardo.pinto@lightbend.com>
2022-09-12 23:52:31 +0200

    chore: adding new event-sourced sample with Spring SDK

---
+++ samples/spring-eventsourced-counter/src/it/java/com/example/CounterIntegrationTest.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import kalix.springsdk.KalixIntegrationTestKitSupport;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Main.class)
+public class CounterIntegrationTest extends KalixIntegrationTestKitSupport {
+
+    @Autowired
+    private WebClient webClient;
+
+    private Duration timeout = Duration.of(10, SECONDS);
+
+    @Test
+    public void verifyCounterEventSourcedWiring() {
+
+        String counterIncrease =
+                webClient
+                        .post()
+                        .uri("/counter/hello/increase/10")
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .block(timeout);
+
+        Assertions.assertEquals("\"10\"", counterIncrease);
+
+        String counterMultiply =
+                webClient
+                        .post()
+                        .uri("/counter/hello/multiply/20")
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .block(timeout);
+
+        Assertions.assertEquals("\"200\"", counterMultiply);
+
+        String counterGet =
+                webClient.get().uri("/counter/hello").retrieve().bodyToMono(String.class).block(timeout);
+
+        Assertions.assertEquals("\"200\"", counterGet);
+    }
+}
