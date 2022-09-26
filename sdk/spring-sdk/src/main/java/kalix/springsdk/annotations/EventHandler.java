6436d05b4538a2416372883d2bc12be6786863e9
Eduardo Pinto <eduardo.pinto@lightbend.com>
2022-09-26 13:40:51 +0100

    docs: add README to springsdk to serve as temporary general docs (#1126)
    * docs: add README to springsdk to serve as temporary general docs
    
    * add views for value entity
    
    * add subscription to ES entities
    
    * adding topics examples
    
    * add javadocs to kalix annotations
    
    * adding cross-component calls example and tiny refactor
    
    * java fmt
    
    * Updated after PR review

--- sdk/spring-sdk/src/main/java/kalix/springsdk/annotations/EventHandler.java
+++ sdk/spring-sdk/src/main/java/kalix/springsdk/annotations/EventHandler.java
@@ -18,6 +18,20 @@ package kalix.springsdk.annotations;
 
 import java.lang.annotation.*;
 
+/**
+ * Annotation for marking a method under EventSourced entities as handlers for events of such
+ * entity.
+ *
+ * <p>A method marked with this annotation needs to:
+ *
+ * <p>
+ *
+ * <ul>
+ *   <li>be public
+ *   <li>have exactly one parameter
+ *   <li>have a return type that matches the state type of the enclosing entity
+ * </ul>
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
--- sdk/spring-sdk/src/main/java/kalix/springsdk/annotations/Subscribe.java
+++ sdk/spring-sdk/src/main/java/kalix/springsdk/annotations/Subscribe.java
@@ -18,28 +18,67 @@ package kalix.springsdk.annotations;
 
 import java.lang.annotation.*;
 
+/**
+ * Annotation for providing ways to subscribe to different streams of information: value entities,
+ * event-sourced entities or topic.
+ */
 public @interface Subscribe {
 
+  /**
+   * Annotation for subscribing to updates from a Value Entity. It can be used both at type and
+   * method levels. When used at type level, it means the `View` will not be transforming state.
+   * When used at method level, it gives the ability to transform the updates into a different state
+   * for the view and the underlying method must be declared to receive one or two parameters:
+   *
+   * <ul>
+   *   <li>when one parameter is passed, the single parameter will be considered the event type such
+   *       method will handle;
+   *   <li>when two parameters are passed, the first one will be considered the view state and the
+   *       second one the event type.
+   * </ul>
+   */
   @Target({ElementType.TYPE, ElementType.METHOD})
   @Retention(RetentionPolicy.RUNTIME)
   @Documented
   @interface ValueEntity {
+    /**
+     * Assign the class type of the entity one intends to subscribe to, which must extend {@link
+     * kalix.javasdk.valueentity.ValueEntity ValueEntity}.
+     */
     Class<? extends kalix.javasdk.valueentity.ValueEntity<?>> value();
   }
 
+  /**
+   * Annotation for subscribing to updates from an Event-sourced Entity. It can be used only at type
+   * level. The underlying method must be declared to receive one or two parameters:
+   *
+   * <ul>
+   *   <li>when one parameter is passed, the single parameter will be considered the event type such
+   *       method will handle;
+   *   <li>when two parameters are passed, the first one will be considered the view state and the
+   *       second one the event type.
+   * </ul>
+   */
   @Target(ElementType.METHOD)
   @Retention(RetentionPolicy.RUNTIME)
   @Documented
   @interface EventSourcedEntity {
+    /**
+     * Assign the class type of the entity one intends to subscribe to, which must extend {@link
+     * kalix.javasdk.eventsourcedentity.EventSourcedEntity EventSourcedEntity}.
+     */
     Class<? extends kalix.javasdk.eventsourcedentity.EventSourcedEntity<?>> value();
   }
 
+  /** Annotation for subscribing to messages from a topic (i.e PubSub or Kafka topic). */
   @Target(ElementType.METHOD)
   @Retention(RetentionPolicy.RUNTIME)
   @Documented
   @interface Topic {
+    /** Assign the name of the topic to consume the stream from. */
     String value();
 
+    /** Assign the consumer group name to be used on the broker. */
     String consumerGroup() default "";
   }
 }
