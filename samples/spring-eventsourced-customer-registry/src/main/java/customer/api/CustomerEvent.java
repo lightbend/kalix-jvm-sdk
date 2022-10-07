6a67695123e9b34931d0f899a24c9d6b921bd763
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2022-10-07 11:06:32 +0200

    reverting changes

--- samples/spring-eventsourced-customer-registry/src/main/java/customer/api/CustomerEvent.java
+++ samples/spring-eventsourced-customer-registry/src/main/java/customer/api/CustomerEvent.java
@@ -8,23 +8,20 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 import static customer.api.CustomerEvent.*;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-//@JsonSubTypes(
-//    {
-//        @JsonSubTypes.Type(value = CustomerCreated.class, name = "customer-created"),
-//        @JsonSubTypes.Type(value = NameChanged.class, name = "name-changed"),
-//        @JsonSubTypes.Type(value = AddressChanged.class, name = "address-changed")
-//    })
+@JsonSubTypes(
+    {
+        @JsonSubTypes.Type(value = CustomerCreated.class, name = "customer-created"),
+        @JsonSubTypes.Type(value = NameChanged.class, name = "name-changed"),
+        @JsonSubTypes.Type(value = AddressChanged.class, name = "address-changed")
+    })
 public sealed interface CustomerEvent {
 
-  @JsonTypeName("customer-created")
   record CustomerCreated(String email, String name, Address address) implements CustomerEvent {
   }
 
-  @JsonTypeName("name-changed")
   record NameChanged(String newName) implements CustomerEvent {
   }
 
-  @JsonTypeName("address-changed")
   record AddressChanged(Address address) implements CustomerEvent {
   }
 }
