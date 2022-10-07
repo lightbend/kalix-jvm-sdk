25a9bb4c92250e59c4c0f276165b04f170986e4c
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2022-10-07 15:50:14 +0200

    missing jackson annotations

--- samples/spring-eventsourced-counter/src/main/java/com/example/CounterEvent.java
+++ samples/spring-eventsourced-counter/src/main/java/com/example/CounterEvent.java
@@ -1,5 +1,16 @@
 package com.example;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import static com.example.CounterEvent.*;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes(
+    {
+        @JsonSubTypes.Type(value = ValueIncreased.class, name = "value-increased"),
+        @JsonSubTypes.Type(value = ValueMultiplied.class, name = "value-multiplied"),
+    })
 public sealed interface CounterEvent {
 
   record ValueIncreased(int value) implements CounterEvent {
