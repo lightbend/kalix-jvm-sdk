2ff4cfb8c1fc4153de6cc6b1738be7de4d3c6668
Johan Andrén <johan@markatta.com>
2022-10-12 17:20:24 +0200

    Comment out ACL by default to simplify running locally

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Kalix {
     val ProtocolVersionMajor = 1
     val ProtocolVersionMinor = 0
-    val ProxyVersion = System.getProperty("kalix-proxy.version", "1.0.20-31-3e0ef328-SNAPSHOT") // FIXME non-snapshot with service2service eventing
+    val ProxyVersion = System.getProperty("kalix-proxy.version", "1.0.20-42-d6bff788-SNAPSHOT") // FIXME non-snapshot with service2service eventing
   }
 
   // changing the Scala version of the Java SDK affects end users
