00cb0b72464115b8b55a480dcc55daf6553ddade
Johan Andrén <johan@markatta.com>
2022-10-14 14:06:30 +0200

    chore: Bump jackson databind (#1170)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val AkkaHttpVersion = "10.2.10" // Note: should at least the Akka HTTP version required by Akka gRPC
   val ScalaTestVersion = "3.2.14"
   val JacksonVersion = "2.13.4"
-  val JacksonDatabindVersion = "2.13.4"
+  val JacksonDatabindVersion = "2.13.4.1"
   val DockerBaseImageVersion = "adoptopenjdk/openjdk11:debianslim-jre"
   val LogbackVersion = "1.2.11"
   val LogbackContribVersion = "0.1.5"
