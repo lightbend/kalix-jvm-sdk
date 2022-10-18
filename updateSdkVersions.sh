5caca758e5bd910f7c58e67cb1b32770c511ba47
Eduardo Pinto <eduardo.pinto@lightbend.com>
2022-10-18 15:15:56 +0100

    chore: fix automate PR for sdk bump (#1179)

--- updateSdkVersions.sh
+++ updateSdkVersions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # this script is meant to be used after a new SDK version is out
 # to facilitate the update of all the places where we usually depend on the latest version
@@ -9,13 +9,19 @@ if [[ -z "$SDK_VERSION" ]]; then
     exit 1
 fi
 
+updateDocs() {
+  echo ">>> Update references in docs"
+  sed -i.bak "s/<kalix-sdk.version>\(.*\)<\/kalix-sdk.version>/<kalix-sdk.version>$SDK_VERSION<\/kalix-sdk.version>/" ./docs/src/modules/java/pages/spring-client.adoc
+  rm docs/src/modules/java/pages/spring-client.adoc.bak
+}
+
 updateJavaSamples() {
   echo ">>> Updating pom versions to $SDK_VERSION"
   PROJS=$(find $1 -type f -name "pom.xml")
   for i in ${PROJS[@]}
   do
     echo "Updating pom for: $i"
-    sed -i .bak "s/<kalix-sdk.version>\(.*\)<\/kalix-sdk.version>/<kalix-sdk.version>$SDK_VERSION<\/kalix-sdk.version>/" $i
+    sed -i.bak "s/<kalix-sdk.version>\(.*\)<\/kalix-sdk.version>/<kalix-sdk.version>$SDK_VERSION<\/kalix-sdk.version>/" $i
     rm $i.bak
   done
 }
@@ -26,14 +32,14 @@ updateScalaSamples() {
   for i in ${PROJS[@]}
   do
     echo "Updating plugins sbt for: $i"
-    sed -i .bak "s/System.getProperty(\"kalix-sdk.version\", \".*\"))/System.getProperty(\"kalix-sdk.version\", \"$SDK_VERSION\"))/" $i
+    sed -i.bak "s/System.getProperty(\"kalix-sdk.version\", \".*\"))/System.getProperty(\"kalix-sdk.version\", \"$SDK_VERSION\"))/" $i
     rm $i.bak
   done
 }
 
 updateMavenPlugin() {
   echo ">>> Updating maven plugin to $SDK_VERSION"
-  cd maven-java && mvn versions:set -DnewVersion="$SDK_VERSION"
+  cd maven-java && mvn versions:set -DnewVersion="$SDK_VERSION" && cd ..
 }
 
 DEFAULT_SAMPLES="./samples"
@@ -46,11 +52,16 @@ case ${option} in
       ;;
    plugin) updateMavenPlugin
       ;;
+   docs) updateDocs
+      ;;
    all)
-     updateJavaSamples $sample; updateScalaSamples $sample; updateMavenPlugin
+     updateDocs
+     updateJavaSamples $sample
+     updateScalaSamples $sample
+     updateMavenPlugin
       ;;
    *)
-      echo "`basename ${0}`:usage: java|scala|plugin|all [project-folder]"
+      echo "`basename ${0}`:usage: java|scala|plugin|docs|all [project-folder]"
       echo "e.g.: `basename ${0}` java ./samples/java-customer-registry-kafka-quickstart/"
       exit 1 # Command to come out of the program with status 1
       ;;
