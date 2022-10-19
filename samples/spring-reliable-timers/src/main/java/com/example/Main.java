35c2714db965bd2856a2f1ec03cc9f058ce0c755
Eduardo Pinto <eduardo.pinto@lightbend.com>
2022-10-19 11:47:20 +0100

    chore: adding acl to spring reliable timers sample

--- samples/spring-reliable-timers/src/main/java/com/example/Main.java
+++ samples/spring-reliable-timers/src/main/java/com/example/Main.java
@@ -9,6 +9,11 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(KalixConfiguration.class)
+// NOTE: This default ACL settings is very permissive as it allows any traffic from the internet.
+// Our samples default to this permissive configuration to allow users to easily try it out.
+// However, this configuration is not intended to be reproduced in production environments.
+// Documentation at https://docs.kalix.io/services/using-acls.html
+@Acl(allow = @Acl.Matcher(principal = Acl.Principal.ALL))
 public class Main {
 
   private static final Logger logger = LoggerFactory.getLogger(Main.class);
