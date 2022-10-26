2cd936aa5987ecf5ce771c8dc38cd48af285f85d
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2022-10-26 16:08:40 +0300

    feat: adding actions spring docs #7719 (#1205)
    * feat: adding actions spring docs #7719

--- samples/spring-fibonacci-action/src/main/java/com/example/fibonacci/FibonacciAction.java
+++ samples/spring-fibonacci-action/src/main/java/com/example/fibonacci/FibonacciAction.java
@@ -1,11 +1,20 @@
 package com.example.fibonacci;
 
-import kalix.javasdk.action.Action;
+
 
 import java.util.function.Predicate;
+// tag::implementing-action[]
+import kalix.javasdk.action.Action;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
+// end::implementing-action[]
+
+// tag::implementing-action[]
 @RequestMapping("/fibonacci")
 public class FibonacciAction extends Action { 
 
@@ -16,24 +25,25 @@ public class FibonacciAction extends Action {
     };
     return isPerfectSquare.test(5*num*num + 4) || isPerfectSquare.test(5*num*num - 4);
   }
-  private long nextFib(long num) {
+  private long nextFib(long num) {   // <2>
     double result = num * (1 + Math.sqrt(5)) / 2.0;
     return Math.round(result);
   }
 
   @GetMapping("/{number}/next")
-  public Effect<Number> nextNumber(@PathVariable Long number) {
+  public Effect<Number> nextNumber(@PathVariable Long number) { // <3>
     return nextNumber(new Number(number));
   }
 
   @PostMapping("/next")
-  public Effect<Number> nextNumber(@RequestBody Number number) {
+  public Effect<Number> nextNumber(@RequestBody Number number) {  
     long num =  number.value();
-    if (isFibonacci(num)) {
+    if (isFibonacci(num)) {                                     // <4>
       return effects().reply(new Number(nextFib(num)));
     } else {
-      return effects()
+      return effects()                                          // <5>
           .error("Input number is not a Fibonacci number, received '" + num + "'");
     }
   }
 }
+// end::implementing-action[]
