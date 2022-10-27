076a38173884700003518f62693105016be75a1e
francisco <franciscolopezsancho@gmail.com>
2022-10-27 16:02:00 +0300

    feat: adding pub sub spring docs #7722

--- samples/spring-eventsourced-counter/src/main/java/com/example/actions/CounterJournalToTopicAction.java
+++ samples/spring-eventsourced-counter/src/main/java/com/example/actions/CounterJournalToTopicAction.java
@@ -1,11 +1,25 @@
 package com.example.actions;
 
-// tag::pubsub-action[]
+// tag::sub-ESE-pub-topic-action[]
+// tag::sub-ESE-action[]
+// tag::sub-topic-action[]
 import kalix.javasdk.action.Action;
-import kalix.springsdk.annotations.Publish;
-import kalix.springsdk.annotations.Subscribe;
+// end::sub-ESE-pub-topic-action[]
+// end::sub-ESE-action[]
+// end::sub-topic-action[]
 
-// end::pubsub-action[]
+// tag::sub-ESE-pub-topic-action[]
+import kalix.springsdk.annotations.Publish;
+// end::sub-ESE-pub-topic-action[]
+
+// tag::sub-ESE-pub-topic-action[]
+// tag::sub-ESE-action[]
+// tag::sub-topic-action[]
+import kalix.springsdk.annotations.Subscribe;
+// end::sub-ESE-pub-topic-action[]
+// end::sub-ESE-action[]
+// end::sub-topic-action[]
+
 import com.example.CounterEvent;
 import com.example.CounterEvent.ValueIncreased;
 import com.example.CounterEvent.ValueMultiplied;
@@ -14,27 +28,67 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //TODO move to type level the subscription when SDK is released
 
-// tag::pubsub-action[]
-public class CounterJournalToTopicAction extends Action {
-    private Logger logger = LoggerFactory.getLogger(CounterJournalToTopicAction.class);
-    // tag::pubsub-action[]
-    @Subscribe.EventSourcedEntity(value = Counter.class) // <1>
-    @Publish.Topic("increasedTopic") // <2>
-    public Action.Effect<CounterEvent> onValueIncreased(ValueIncreased valueIncreased){ // <3>
-       ValueIncreased vi = new ValueIncreased(valueIncreased.value() + 1);
-       return effects().reply(vi);
-    }
-    // end::pubsub-action[]
-    @Subscribe.EventSourcedEntity(value = Counter.class) // <4>
-    public Action.Effect<Integer> onValueMultiplied(ValueMultiplied valueMultiplied){ // <5>
-        logger.info("Received multiplied event: " + valueMultiplied.toString()); // <6>
-        return effects().reply(0);
-    }
+// tag::sub-ESE-pub-topic-action[]
+// tag::sub-ESE-action[]
+// tag::sub-topic-action[]
 
-    @Subscribe.Topic("increasedTopic") // <7>
-    public Action.Effect<Integer> onEvent(ValueIncreased valueIncreased){
-        logger.info("Received increase event: " + valueIncreased.toString()); // <8>
-        return effects().reply(0);  // <9>
+public class CounterJournalToTopicAction extends Action {
+// end::sub-ESE-pub-topic-action[]
+// end::sub-ESE-action[]
+// end::sub-topic-action[]
+    
+
+
+    // tag::sub-ESE-action[]
+    // tag::sub-topic-action[]
+    private Logger logger = LoggerFactory.getLogger(CounterJournalToTopicAction.class);
+
+    // end::sub-ESE-action[]
+    // end::sub-topic-action[]
+
+    // tag::sub-ESE-pub-topic-action[] 
+    @Subscribe.EventSourcedEntity(value = Counter.class) // <1>
+    @Publish.Topic("myTopic") // <2>
+    public Action.Effect<CounterEvent> onEntityEventToTopic(CounterEvent event){ // <3>
+        if (event instanceof ValueIncreased){
+            ValueIncreased vi = new ValueIncreased(((ValueIncreased) event).value() + 1);
+            return effects().reply(vi);
+        } else if (event instanceof ValueMultiplied){
+            ValueMultiplied vm = new ValueMultiplied(((ValueMultiplied) event).value()*2);
+            return effects().reply(vm);
+        } else return effects().reply(event); // <4>
     }
+    // end::sub-ESE-pub-topic-action[]
+
+    // tag::sub-ESE-action[]
+    @Subscribe.EventSourcedEntity(value = Counter.class) // <1>
+    public Action.Effect<Integer> onEntityEvent(CounterEvent event){ // <2>
+        if (event instanceof ValueIncreased){
+            logger.info("Received increased event: " + event.toString());
+        } else if (event instanceof ValueMultiplied){
+            logger.info("Received multiplied event: " + event.toString());
+        }
+        return effects().reply(0); // <3>
+    }
+    // end::sub-ESE-action[]
+
+    // tag::sub-topic-action[]
+    @Subscribe.Topic(value = "myTopic") // <1>
+    public Action.Effect<Integer> onTopicEvent(CounterEvent event){ // <2>
+        if (event instanceof ValueIncreased){
+            logger.info("Received increased event: " + event.toString());
+        } else if (event instanceof ValueMultiplied){
+            logger.info("Received multiplied event: " + event.toString());
+        }
+        return effects().reply(0); // <3>
+    }
+    // end::sub-topic-action[]
+
+
+// tag::sub-ESE-pub-topic-action[]
+// tag::sub-ESE-action[]
+// tag::sub-topic-action[]
 }
-// end::pubsub-action[]
+// end::sub-ESE-pub-topic-action[]
+// end::sub-ESE-action[]
+// end::sub-topic-action[]
