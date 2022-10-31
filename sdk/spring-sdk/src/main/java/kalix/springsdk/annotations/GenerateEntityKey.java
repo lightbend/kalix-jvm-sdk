af9c610133f80e755dec596d25396f7d7da62ffb
Renato Cavalcanti <renato@cavalcanti.be>
2022-10-31 09:07:31 +0100

    Update sdk/spring-sdk/src/main/java/kalix/springsdk/annotations/GenerateEntityKey.java
    Co-authored-by: Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>

--- sdk/spring-sdk/src/main/java/kalix/springsdk/annotations/GenerateEntityKey.java
+++ sdk/spring-sdk/src/main/java/kalix/springsdk/annotations/GenerateEntityKey.java
@@ -21,8 +21,8 @@ import java.lang.annotation.*;
 /**
  * Indicates that Kalix should generate an entity key when this method is invoked.
 
- * The generated entity key will be accessible via the entity context supplied in the SDK.
- * A method annotated with this annotations should not be annotated with {@link EntityKey},
+ * The generated entity key is accessible via the entity context supplied in the SDK.
+ * A method annotated with this annotation should not be annotated with {@link EntityKey},
  * if it does, an error will be raised.
  *
  * The generated key will be a Version 4 (random) UUID. The UUID will be generated using a cryptographically secure
