523616262aff0cfa0b9ce22f518436f8308327a7
francisco <franciscolopezsancho@gmail.com>
2022-11-10 18:10:18 +0300

    addressing comments and fixing link in Main

--- samples/spring-customer-registry-views-quickstart/src/main/java/customer/Main.java
+++ samples/spring-customer-registry-views-quickstart/src/main/java/customer/Main.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Import;
 // NOTE: This default ACL settings is very permissive as it allows any traffic from the internet.
 // Our samples default to this permissive configuration to allow users to easily try it out.
 // However, this configuration is not intended to be reproduced in production environments.
-// Documentation at https://docs.kalix.io/services/using-acls.html
+// Documentation at https://docs.kalix.io/spring/using-acls.html
 @Acl(allow = @Acl.Matcher(principal = Acl.Principal.INTERNET))
 public class Main {
 
