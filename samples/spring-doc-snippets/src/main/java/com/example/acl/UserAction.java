e205dd78f72984de4f544c726385cecab1c5facd
francisco <franciscolopezsancho@gmail.com>
2022-11-30 10:59:01 +0300

    moving snippets to sample-doc-snippets

---
+++ samples/spring-doc-snippets/src/main/java/com/example/acl/UserAction.java
@@ -0,0 +1,10 @@
+package com.example.acl;
+
+import kalix.javasdk.action.Action;
+import kalix.springsdk.annotations.Acl;
+// tag::acl[]
+@Acl(denyCode = 5)
+public class UserAction extends Action {
+    //...
+}
+// end::acl[]
