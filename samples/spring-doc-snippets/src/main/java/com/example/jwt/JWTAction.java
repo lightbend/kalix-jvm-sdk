9646202f6c24a0d44b1ff1bb4b2d01460a951b8e
francisco <franciscolopezsancho@gmail.com>
2022-12-02 18:22:54 +0300

    addressing comments about indentation

--- samples/spring-doc-snippets/src/main/java/com/example/jwt/JWTAction.java
+++ samples/spring-doc-snippets/src/main/java/com/example/jwt/JWTAction.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class JWTAction extends Action {
 
     // tag::bearer-token[]
-        @PostMapping("/message")
-        @JWT(validate = JWT.JwtMethodMode.BEARER_TOKEN) // <1>
-        public Action.Effect<String> message(@RequestBody String msg) {
+    @PostMapping("/message")
+    @JWT(validate = JWT.JwtMethodMode.BEARER_TOKEN) // <1>
+    public Action.Effect<String> message(@RequestBody String msg) {
             return effects().reply(msg);
         }
     // end::bearer-token[]
