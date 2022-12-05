90a9fc44a0e5cd311bde962cfebe7731c3fb7a6e
Renato Cavalcanti <renato@cavalcanti.be>
2022-12-05 15:55:41 +0100

    chore: use more explicit name for colleciton result (#1304)

--- samples/spring-valueentity-customer-registry/src/main/java/com/example/api/CustomersResponse.java
+++ samples/spring-valueentity-customer-registry/src/main/java/com/example/api/CustomersResponse.java
@@ -5,5 +5,5 @@ import com.example.domain.Customer;
 import java.util.Collection;
 
 // tag::record[]
-public record CustomersResponse(Collection<Customer> results) { }
+public record CustomersResponse(Collection<Customer> customers) { }
 // end::record[]
