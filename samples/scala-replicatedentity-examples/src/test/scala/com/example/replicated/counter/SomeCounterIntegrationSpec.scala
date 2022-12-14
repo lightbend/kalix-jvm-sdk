f1b80421b19bcc86883c4ade282e967776f275eb
Eduardo Pinto <eduardo.pinto@lightbend.com>
2022-12-14 21:54:13 +0000

    add removal note in IT test

--- samples/scala-replicatedentity-examples/src/test/scala/com/example/replicated/counter/SomeCounterIntegrationSpec.scala
+++ samples/scala-replicatedentity-examples/src/test/scala/com/example/replicated/counter/SomeCounterIntegrationSpec.scala
@@ -41,6 +41,7 @@ class SomeCounterIntegrationSpec extends AnyWordSpec with Matchers with BeforeAn
       getResult.futureValue.value shouldBe (42 - 32)
     }
 
+    // FIXME remove test once TCK tests are updated to include replicated entities
     "Test value after passivation" in {
       val counterId = "1"
 
