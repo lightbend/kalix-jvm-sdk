a50303603d9eac2c691bb44d1b14570254c7eadf
Renato Cavalcanti <renato@cavalcanti.be>
2023-01-03 15:05:50 +0100

    revert badly formatted file

--- sdk/java-sdk/src/test/java/kalix/javasdk/valueentity/CartEntity.java
+++ sdk/java-sdk/src/test/java/kalix/javasdk/valueentity/CartEntity.java
@@ -57,7 +57,8 @@ public class CartEntity extends AbstractCartEntity {
         removeItemByProductId(currentState, addLineItem.getProductId());
     lineItems.add(lineItem);
     lineItems.sort(Comparator.comparing(ShoppingCartDomain.LineItem::getProductId));
-    return effects()        .updateState(ShoppingCartDomain.Cart.newBuilder().addAllItems(lineItems).build())
+    return effects()
+        .updateState(ShoppingCartDomain.Cart.newBuilder().addAllItems(lineItems).build())
         .thenReply(Empty.getDefaultInstance());
   }
 
