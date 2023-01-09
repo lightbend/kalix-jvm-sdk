0b9cb077fd466b7e6cac4ee164b245d1de6eeeca
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2023-01-09 14:33:49 +0100

    header

--- sdk/spring-sdk/src/it/java/com/example/wiring/actions/echo/ActionWithMetadata.java
+++ sdk/spring-sdk/src/it/java/com/example/wiring/actions/echo/ActionWithMetadata.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Lightbend Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.wiring.actions.echo;
 
 import kalix.javasdk.Metadata;
@@ -9,7 +25,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.nio.ByteBuffer;
 import java.util.List;
 
-public class ActionWithMetadata extends Action{
+public class ActionWithMetadata extends Action {
 
   private KalixClient kalixClient;
 
@@ -18,14 +34,14 @@ public class ActionWithMetadata extends Action{
   }
 
   @GetMapping("/action-with-meta/{key}/{value}")
-  public Effect<Message> actionWithMeta(@PathVariable String key, @PathVariable String value ){
-    var def = kalixClient.get("/return-meta/"+key, Message.class);
+  public Effect<Message> actionWithMeta(@PathVariable String key, @PathVariable String value) {
+    var def = kalixClient.get("/return-meta/" + key, Message.class);
     return effects().forward(def.withMetadata(List.of(stringValue(key, value))));
   }
 
 
   @GetMapping("/return-meta/{key}")
-  public Effect<Message> returnMeta(@PathVariable String key){
+  public Effect<Message> returnMeta(@PathVariable String key) {
     var metaValue = actionContext().metadata().get(key).get();
     return effects().reply(new Message(metaValue));
   }
