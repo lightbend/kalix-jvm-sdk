64a811fb367d155ff534af2ec32facbcc1405b49
francisco <franciscolopezsancho@gmail.com>
2023-02-14 18:19:56 +0100

    Adding opt-in deploy with two variables

--- maven-java/kalix-maven-plugin/src/main/java/kalix/DeployMojo.java
+++ maven-java/kalix-maven-plugin/src/main/java/kalix/DeployMojo.java
@@ -7,11 +7,10 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Goal which deploys the current project to Kalix.
@@ -36,8 +35,8 @@ public class DeployMojo extends AbstractMojo {
     @Parameter(property = "kalixContext")
     private String kalixContext;
 
-    @Parameter(property = "projectToDeploy", required = true)
-    private String kalixProjectToDeploy;
+    @Parameter(property = "kalixCurrentProject", required = true)
+    private String kalixCurrentProject;
 
     @Parameter(defaultValue = "false", property = "kalixDeploymentOn", required = true)
     private Boolean kalixDeploymentOn;
@@ -45,41 +44,82 @@ public class DeployMojo extends AbstractMojo {
     @Parameter(defaultValue = "30000", property = "cliTimeoutMs", required = true)
     private Long cliTimeoutMs;
 
-    @Component
-    private Prompter prompter;
-
     private final Log log = getLog();
 
     /**
-     * We deploy by invoking the services deploy command
+     * We deploy by invoking the services deploy command only when `kalixDeploymentOn` is `true`
+     * and the current Kalix project matches with 'kalixCurrentProject'.
      */
     public void execute() throws MojoExecutionException {
-            final List<String> commandLine;
-            final int deploymentResult;
-            if (kalixContext != null && kalixDeploymentOn) {
-                commandLine = Arrays.asList(kalixPath, "--context", kalixContext, "service", "deploy", service, dockerImage);
-                deploymentResult = deploy(commandLine);
-            } else if (kalixDeploymentOn){
-                commandLine = Arrays.asList(kalixPath, "service", "deploy", service, dockerImage);
-                deploymentResult = deploy(commandLine);
-            } 
-            if (deploymentResult == 0) {
-                log.info("Done.");
-            } else {
-                log.error("Unable to deploy. Ensure you can deploy by using the kalix command line directly.");
-            }
+       final List<String> commandLine;
+       final int deploymentResult = 0;
+       if (!Boolean.valueOf(kalixDeploymentOn)){
+           log.info("The variable `kalixDeploymentOn` hasn't been set to `true`. Therefore, not deploying to Kalix");
+           log.info("To deploy to Kalix via `mvn deploy` you need to set `kalixDeploymentOn` to `true` " +
+                   "and `kalixCurrentProject` to your current Kalix project (`kalix config get-project`)");
+           return;
+       }
+       if (!currentKalixProjectMatches()){
+           return;
+       }
+       if (kalixContext != null) {
+           commandLine = Arrays.asList(kalixPath, "--context", kalixContext, "service", "deploy", service, dockerImage);
+           deploy(commandLine);
+           } else {
+           commandLine = Arrays.asList(kalixPath, "service", "deploy", service, dockerImage);
+           deploy(commandLine);
+       }
     }
 
-    public int deploy(List<String> commandLine) throws MojoExecutionException {
+    private boolean currentKalixProjectMatches() throws MojoExecutionException {
+      try {
+          List<String> commandLine = Arrays.asList(kalixPath, "config", "get-project");
+          Process process = new ProcessBuilder().command(commandLine).start();
+          synchronized (process) {
+            process.wait(cliTimeoutMs);
+          }
+          final int getProjectResult = process.exitValue();
+          if (getProjectResult == 0){
+              InputStream inputStream = process.getInputStream();
+              Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+              String currentProjectInfo = "";
+              while(scanner.hasNext()){
+                  currentProjectInfo += scanner.next();
+              }
+              if(!currentProjectInfo.contains(kalixCurrentProject) || kalixCurrentProject.isEmpty()){
+                  log.info("Your current Kalix project ID is not [" + kalixCurrentProject + "]. Therefore, not deploying to Kalix");
+                  log.info("Your current Kalix project (`kalix config get-project`) is:");
+                  int newLine = currentProjectInfo.indexOf("\n");
+                  log.info(currentProjectInfo.substring(0, newLine));
+                  log.info(currentProjectInfo.substring(newLine + 1));
+                  return false;
+              }
+              return true;
+          } else {
+              log.error("Unable to run `kalix config get-project`. Ensure you have installed Kalix CLI.");
+              return false;
+          }
+        } catch (IOException | InterruptedException e) {
+            throw new MojoExecutionException("There was a problem deploying", e);
+        }
+    }
+
+    private void deploy(List<String> commandLine) throws MojoExecutionException {
         try {
             log.info("Deploying project to Kalix");
             log.info("Executing `" + String.join(" ", commandLine) + "`");
             Process process = new ProcessBuilder().directory(baseDir).command(commandLine).start();
             synchronized (process) {
                 process.wait(cliTimeoutMs);
-            } 
-            return process.exitValue();
+            }
+            final int deploymentResult = process.exitValue();
+            if (deploymentResult == 0) {
+                log.info("Done.");
+            } else {
+                log.error("Unable to deploy. Ensure you can deploy by using the kalix command line directly.");
+            }
         } catch (IOException | InterruptedException e) {
             throw new MojoExecutionException("There was a problem deploying", e);
         }
+    }
 }
