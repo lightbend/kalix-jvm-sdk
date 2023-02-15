9900600ea65cdfd58cb07abca2f425703ab153ee
Renato Cavalcanti <renato@cavalcanti.be>
2023-02-15 21:59:25 +0100

    updated documentation sections

--- publishLocally.sh
+++ publishLocally.sh
@@ -1,18 +1,7 @@
+# This script will publish the current snapshot of all artifacts. 
+# Including the maven plugin and archetypes.
 
-# this script updates all maven projects versions (maven-java and samples) to align with the current sdk version
-# if SDK_VERSION env var is defined, it will use it, otherwise it will take the version from sbt
-# after running this script, you may run local tests or simply send a PR with the updates. 
-
-# you can pass the path to a sample to only change the version for single sample, 
-# eg: ./updatePomVersions.sh samples/java-eventsourced-counter
-# or you can pass 'all' to it and all java samples (pom.xml) will be updated
-# eg: ./updatePomVersions.sh all
-# useful when testing out new functionality locally.
-
-
-if [ -z ${SDK_VERSION+x} ]; then 
-  SDK_VERSION=$(sbt "print sdkJava/version" | tail -1)
-fi
+SDK_VERSION=$(sbt "print sdkJava/version" | tail -1)
 
 echo
 echo "------------------------------------------------------------------------"
--- updateSamplesVersions.sh
+++ updateSamplesVersions.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
+# publish current snapshot locally and updates passed in samples to use the snapshot.
+# this is mainly used for local tests. Watch out to no commit modified files.
 
-  if [ $# -eq 0 ]; then 
-    echo "No arguments provided!"
-    echo "This scripted is used to update sample versions to the latest snapshot."
-    echo "You should call it by passing one or more samples as arguments."
-    echo "eg: ./updateSampleVersions.sh samples/java-valueentity-counter samples/scala-valueentity-counter"
-    echo "or simply using bash expansion..."
-    echo "eg: ./updateSampleVersions.sh samples/java-*"
-  else 
-    source publishLocally.sh
-    export SDK_VERSION="$SDK_VERSION"
-  
-    echo "------------------------------------------------------------------------"
-    for i in "$@"
-    do
-      echo
-      if [ -f $i/pom.xml ]; then
-        echo "Updating pom.xml file in: $i"
-        sh ./updateSdkVersions.sh java $i
-      elif [ -f $i/build.sbt ]; then
-        echo "Updating plugins.sbt file in: $i"
-        sh ./updateSdkVersions.sh scala $i
-      fi
-    done
-    echo "------------------------------------------------------------------------"
-  fi
+if [ $# -eq 0 ]; then 
+  echo "No arguments provided!"
+  echo "This scripted is used to update sample versions to the latest snapshot."
+  echo "You should call it by passing one or more samples as arguments."
+  echo "eg: ./updateSampleVersions.sh samples/java-valueentity-counter samples/scala-valueentity-counter"
+  echo "or simply using bash expansion..."
+  echo "eg: ./updateSampleVersions.sh samples/java-*"
+else 
+  source publishLocally.sh
+  export SDK_VERSION="$SDK_VERSION"
+
+  echo "------------------------------------------------------------------------"
+  for i in "$@"
+  do
+    echo
+    if [ -f $i/pom.xml ]; then
+      echo "Updating pom.xml file in: $i"
+      sh ./updateSdkVersions.sh java $i
+    elif [ -f $i/build.sbt ]; then
+      echo "Updating plugins.sbt file in: $i"
+      sh ./updateSdkVersions.sh scala $i
+    fi
+  done
+  echo "------------------------------------------------------------------------"
+fi
