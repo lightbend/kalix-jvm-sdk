84620106e58e2f82b4869fde909eb4248afea83f
Renato Cavalcanti <renato@cavalcanti.be>
2023-02-15 22:41:22 +0100

    chore: minor improvement to updatePomVersions.sh script (#1021)
    * chore: publishLocally and updateSamplesVersions.sh
    
    * updated documentation sections

---
+++ publishLocally.sh
@@ -0,0 +1,27 @@
+# This script will publish the current snapshot of all artifacts. 
+# Including the maven plugin and archetypes.
+
+SDK_VERSION=$(sbt "print sdkJava/version" | tail -1)
+
+echo
+echo "------------------------------------------------------------------------"
+echo "Publishing version $SDK_VERSION"
+echo "------------------------------------------------------------------------"
+
+sbt 'publishM2; publishLocal'
+(
+  cd maven-java
+  mvn versions:set -DnewVersion=$SDK_VERSION
+  mvn clean install
+
+  # cleanup
+  rm pom.xml.versionsBackup
+  rm */pom.xml.versionsBackup
+
+  # revert
+  git checkout pom.xml
+  git checkout */pom.xml
+)
+
+
+echo $SDK_VERSION
