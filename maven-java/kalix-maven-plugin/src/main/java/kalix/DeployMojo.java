c39a444c96541305812df3970da42030ded9e7ec
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2023-02-16 19:29:17 +0100

    now deploying to the project when set, irrespective of the local current-project

--- maven-java/kalix-maven-plugin/src/main/java/kalix/DeployMojo.java
+++ maven-java/kalix-maven-plugin/src/main/java/kalix/DeployMojo.java
@@ -3,21 +3,18 @@ package kalix;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 /**
  * Goal which deploys the current project to Kalix.
  */
 @SuppressWarnings("unused")
-@Mojo(name = "deploy", defaultPhase = LifecyclePhase.DEPLOY)
+@Mojo(name = "deploy")
 public class DeployMojo extends AbstractMojo {
 
     @SuppressWarnings("unused")
@@ -36,11 +33,8 @@ public class DeployMojo extends AbstractMojo {
     @Parameter(property = "kalixContext")
     private String kalixContext;
 
-    @Parameter(property = "kalixCurrentProject", required = true)
-    private String kalixCurrentProject;
-
-    @Parameter(defaultValue = "false", property = "deployToKalix", required = true)
-    private Boolean deployToKalix;
+    @Parameter(property = "kalixProject")
+    private String kalixProject;
 
     @Parameter(defaultValue = "30000", property = "cliTimeoutMs", required = true)
     private Long cliTimeoutMs;
@@ -54,72 +48,17 @@ public class DeployMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException {
        final List<String> commandLine;
        final int deploymentResult = 0;
-       if (!Boolean.valueOf(deployToKalix)){
-           log.info("The variable `deployToKalix` hasn't been set to `true`. Therefore, not deploying to Kalix");
-           log.info("To deploy to Kalix via `mvn deploy` you need to set `deployToKalix` to `true` " +
-                   "and `kalixCurrentProject` to your current Kalix project Name (`kalix config get-project`)");
-           return;
-       }
-       if (!currentKalixProjectMatches()){
+       if (kalixProject.isEmpty()){
+           log.info("The variable `kalixProject` hasn't been set. Therefore, not deploying to Kalix");
            return;
        }
        if (kalixContext != null) {
-           commandLine = Arrays.asList(kalixPath, "--context", kalixContext, "service", "deploy", service, dockerImage);
+           commandLine = Arrays.asList(kalixPath, "--context", kalixContext,"--project", kalixProject, "service", "deploy", service, dockerImage);
            deploy(commandLine);
            } else {
-           commandLine = Arrays.asList(kalixPath, "service", "deploy", service, dockerImage);
+           commandLine = Arrays.asList(kalixPath,"--project", kalixProject, "service", "deploy", service, dockerImage);
            deploy(commandLine);
        }
-    }
-
-    private boolean currentKalixProjectMatches() throws MojoExecutionException {
-      try {
-          List<String> commandLine = Arrays.asList(kalixPath, "config", "get-project");
-          Process process = new ProcessBuilder().command(commandLine).start();
-          synchronized (process){
-              process.wait(cliTimeoutMs);
-          }
-          final int getProjectResult = process.exitValue();
-          if (getProjectResult == 0){
-              InputStream inputStream = process.getInputStream();
-              Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
-              String currentProjectInfo = "";
-              while(scanner.hasNext()) {
-                  currentProjectInfo += scanner.next();
-              }
-              int newLine = currentProjectInfo.indexOf("\n");
-              String titlesLine = currentProjectInfo.substring(0, newLine);
-              String valuesLine = currentProjectInfo.substring(newLine + 1);
-              String projectName = getName(titlesLine,valuesLine,1);
-
-              if(!projectName.equals(kalixCurrentProject)){
-                  log.info("Your current Kalix project Name is not [" + kalixCurrentProject + "]. Therefore, not deploying to Kalix");
-                  log.info("Your current Kalix project (`kalix config get-project`) is:");
-                  log.info(titlesLine);
-                  log.info(valuesLine);
-                  return false;
-              }
-              return true;
-          } else {
-              log.error("Unable to run `kalix config get-project`. Ensure you have installed Kalix CLI.");
-              return false;
-          }
-        } catch (IOException | InterruptedException e) {
-            throw new MojoExecutionException("There was a problem deploying", e);
-        }
-    }
-
-    private String getName(String titlesLine, String valuesLine, int expectedPosition) throws MojoExecutionException{
-        //First checks that NAME is in the first position when `kalix config get-project`
-        String nameTitle = Arrays.stream(titlesLine.split(" "))
-                .filter( x -> !x.isEmpty()).collect(Collectors.toList()).get(1);
-        if(nameTitle.equals("NAME")){
-            String nameValue = Arrays.stream(valuesLine.split(" "))
-                    .filter( x -> !x.isEmpty()).collect(Collectors.toList()).get(1);
-            return nameValue;
-        }else {
-            throw new MojoExecutionException("Can't retrieve the NAME of your project. You probably have an incompatibility with the Kalix Maven plugin and the Kalix CLI.");
-        }
     }
 
     private void deploy(List<String> commandLine) throws MojoExecutionException {
