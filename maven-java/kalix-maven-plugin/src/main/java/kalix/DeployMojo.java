5a8e9b12acaeb5584b414fe131e197bce4e89b41
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2023-02-23 12:06:59 +0100

    adding user friendly message

--- maven-java/kalix-maven-plugin/src/main/java/kalix/DeployMojo.java
+++ maven-java/kalix-maven-plugin/src/main/java/kalix/DeployMojo.java
@@ -10,6 +10,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 /**
@@ -44,37 +45,45 @@ public class DeployMojo extends AbstractMojo {
     private final Log log = getLog();
 
     /**
-     * We deploy by invoking the services deploy command to 'kalixProject' if set.
+     * Deploys to Kalix if set to 'kalixProject' by invoking the `kalix services deploy` command.
      * If 'kalixProject' is not set then deploying to the currently selected project.
      */
     public void execute() throws MojoExecutionException {
-        if (kalixProject.isEmpty()) {
-            log.info("`kalixProject` hasn't been set. Therefore, deploying to the currently selected project configured via Kalix CLI");
-            deploy(Arrays.asList(kalixPath, "--context", kalixProject, "service", "deploy", service, dockerImage));
-            return;
-        }
-        if (kalixContext != null) {
-            deploy(Arrays.asList(kalixPath, "--context", kalixContext, "--project", kalixProject, "service", "deploy", service, dockerImage);
-        } else {
-            deploy(Arrays.asList(kalixPath, "--project", kalixProject, "service", "deploy", service, dockerImage));
-        }
+       deploy(service, dockerImage,Optional.ofNullable(kalixProject),Optional.ofNullable(kalixContext));
     }
 
-    private void deploy(List<String> commandLine) throws MojoExecutionException {
+    private void deploy(String service, String dockerImage, Optional<String> kalixProject, Optional<String> kalixContext) throws MojoExecutionException {
         try {
-            log.info("Deploying project to Kalix");
-            log.info("Executing `" + String.join(" ", commandLine) + "`");
+            List<String> commandLine = (Arrays.asList(kalixPath, "service", "deploy", service, dockerImage));
+            String messageExtraInfo = "";
+
+            if (kalixProject.isPresent()){
+                commandLine.add("--project");
+                commandLine.add(kalixProject.get());
+                messageExtraInfo += " in project ["+kalixProject.get()+"]";
+            } else {
+                log.info("`kalixProject` hasn't been set. Therefore, deploying to the currently selected project configured via Kalix CLI.");
+            }
+            if (kalixContext.isPresent()){
+                commandLine.add( "--context");
+                commandLine.add(kalixContext.get());
+                messageExtraInfo += " with context ["+kalixContext.get()+"]";
+            }
+
+            log.info("Deploying project to Kalix.");
+            String commandLineString = String.join(" ", commandLine);
+            log.info("Executing `" + commandLineString + "`.");
             Process process = new ProcessBuilder().directory(baseDir).command(commandLine).start();
             synchronized (process) {
                 process.wait(cliTimeoutMs);
             }
             final int deploymentResult = process.exitValue();
             if (deploymentResult == 0) {
-                log.info("Done.");
+                log.info("Successfully deployed service ["+service+"]" + messageExtraInfo + ".");
             } else {
                 InputStream errorStream = process.getErrorStream();
                 Scanner scanner = new Scanner(errorStream, StandardCharsets.UTF_8.name());
-                log.error("Unable to deploy. Ensure you can deploy by using the kalix command line directly.");
+                log.error("Unable to deploy. Error executing `" + commandLineString + "`.");
                 while(scanner.hasNext()){
                     log.error(scanner.useDelimiter("\\A").next().replaceAll("[\\n\\r]", ""));
                 }
