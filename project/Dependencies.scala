87fa76b6a47fe6ec8202ba2abc28284af5cbd331
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2023-03-15 12:48:56 +0100

    dump proxy version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Kalix {
     val ProtocolVersionMajor = 1
     val ProtocolVersionMinor = 1
-    val ProxyVersion = System.getProperty("kalix-proxy.version", "1.1.5-13-267ea14b-dev-SNAPSHOT")
+    val ProxyVersion = System.getProperty("kalix-proxy.version", "1.1.6")
   }
 
   // changing the Scala version of the Java SDK affects end users
