02de058cde719b7abedbd21e3c31e1b9086749b2
Renato Cavalcanti <renato@cavalcanti.be>
2023-04-06 14:02:08 +0200

    default phase to VALIDATE

--- maven-java/kalix-maven-plugin/src/main/scala/kalix/RunMojo.scala
+++ maven-java/kalix-maven-plugin/src/main/scala/kalix/RunMojo.scala
@@ -17,7 +17,7 @@ import org.twdata.maven.mojoexecutor.MojoExecutor._
  *
  * Kalix Server bootstrap can be skipped by setting `runProxy` to false.
  */
-@Mojo(name = "run", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyResolution = ResolutionScope.RUNTIME)
+@Mojo(name = "run", defaultPhase = LifecyclePhase.VALIDATE, requiresDependencyResolution = ResolutionScope.RUNTIME)
 @Execute(phase = LifecyclePhase.COMPILE)
 class RunMojo extends AbstractMojo {
   @Component
