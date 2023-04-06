d2081d35615e2972095111cc40572e327fabd276
Enno Runne <458526+ennru@users.noreply.github.com>
2023-04-06 11:21:46 +0200

    ci: migrate sbt samples (#1563)
    * ci: migrate sbt samples
    
    * Syntax
    
    * Fetch
    
    * Migrate remaining sbt samples
    
    * Don't fail fast
    
    * Correct location
    
    * switch off testing for one sample
    
    * Remove check from Circle; security hardening
    
    * try sbt --client
    
    * sbt --client syntax
    
    * read the correct line of output
    
    * Back to old order
    
    * Sigh
    
    * The output get lost somewhere
    
    * Read version from file
    
    * Somethings off with sbtn and output
    
    * Remove reference to check
    
    * Read version line

---
+++ .github/ci-check-samples.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+ALL_OK=true
+for SAMPLE in samples/java* samples/scala*
+do
+  SAMPLE_NAME="${SAMPLE#samples/}"
+  if [ $(grep -c ${SAMPLE_NAME} .github/workflows/samples.yml) -lt 1 ] ; then
+    echo "${SAMPLE_NAME} is missing CI-tests"
+    ALL_OK=false
+  else
+    echo "${SAMPLE_NAME} found"
+  fi
+done
+$ALL_OK
