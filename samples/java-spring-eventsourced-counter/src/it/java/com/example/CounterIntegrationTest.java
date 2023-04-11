60743e6bee89889565f41a5e70bf4c27bf23e342
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2023-04-11 10:57:00 +0200

    fixing compilation

--- samples/java-spring-eventsourced-counter/src/it/java/com/example/CounterIntegrationTest.java
+++ samples/java-spring-eventsourced-counter/src/it/java/com/example/CounterIntegrationTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
