f926bd7f99a06b77c0643e0ccb19e3d322cc0f6a
Renato Cavalcanti <renato@cavalcanti.be>
2023-04-21 14:39:33 +0200

    feat: local discovery for WebClient (#1583)

---
+++ sdk/java-sdk-protobuf/src/main/scala/kalix/javasdk/impl/HostAndPort.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Lightbend Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kalix.javasdk.impl
+
+object HostAndPort {
+
+  private val HostPortPattern = """(.+):(\d+)""".r
+  private val PortPattern = """(\d+)""".r
+
+  /**
+   * When running locally, users can configure service port mappings associating a name and a port. In such a case, we
+   * will resolve to 0.0.0.0:port and that just enough. However, when build a docker-compose file containing more than
+   * one service, the config will need to include the docker host address, for example:
+   * -Dkalix.dev-mode.service-port-mappings.my-service=host.docker.internal:9001
+   *
+   * We should not default to host.docker.internal because it might depend on docker environment, for example: Docker
+   * Desktop, Podman, Colima, Linux, etc.
+   */
+  def extract(hostPort: String): (String, Int) =
+    hostPort match {
+      case HostPortPattern(h, p) => (h, p.toInt)
+      case PortPattern(p)        => ("0.0.0.0", p.toInt)
+      case _ =>
+        throw new IllegalArgumentException(s"Invalid service port mapping: $hostPort")
+    }
+}
