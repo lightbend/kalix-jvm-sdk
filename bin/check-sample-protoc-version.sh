4d01fe92a5ca5ba75d35cfb31421512075b331ff
Enno Runne <458526+ennru@users.noreply.github.com>
2023-04-27 13:48:11 +0200

    move checks to shell script

---
+++ bin/check-sample-protoc-version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+AKKA_GRPC_PROTOC=$(sbt --client "print coreSdk/protocVersion" 2>/dev/null | tail -n 3 | head -n 1)
+POM_PROTOC_VERSIONS=$(find ./samples ./maven-java/ -name pom.xml | xargs egrep -h "com.google.protobuf:protoc:" | sed 's/.*:protoc://' | sed 's/:exe.*//' | sort | uniq)
+if [ $(echo "$POM_PROTOC_VERSIONS" | wc -l) -ne 1 ]; then
+  echo  "Not all Java Samples an Archetypes have the same protoc version:"
+  find ./samples ./maven-java/ -name pom.xml | xargs egrep "com.google.protobuf:protoc:"
+  false
+elif [ "$AKKA_GRPC_PROTOC" != "$POM_PROTOC_VERSIONS" ]; then
+  echo  "Java Samples and Archetypes ($POM_PROTOC_VERSIONS) does not have the same protoc version as Akka gRPC ($AKKA_GRPC_PROTOC)"
+  false
+fi
