45790c0834e438fe95fb27f633dbd5a9392b490d
Enno Runne <458526+ennru@users.noreply.github.com>
2023-05-03 17:42:10 +0200

    Something is off with -client

--- bin/check-sample-protoc-version.sh
+++ bin/check-sample-protoc-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-AKKA_GRPC_PROTOC=$(sbt --client "print coreSdk/protocVersion" 2>/dev/null | tail -n 3 | head -n 1)
+AKKA_GRPC_PROTOC=$(sbt "print coreSdk/protocVersion" 2>/dev/null | tail -n 1 | head -n 1)
 POM_PROTOC_VERSIONS=$(find ./samples ./maven-java/ -name pom.xml | xargs egrep -h "com.google.protobuf:protoc:" | sed 's/.*:protoc://' | sed 's/:exe.*//' | sort | uniq)
 if [ $(echo "$POM_PROTOC_VERSIONS" | wc -l) -ne 1 ]; then
   echo  "Not all Java Samples an Archetypes have the same protoc version:"
