606cb873f60e4bb9d969b925cc432bda0ebf101a
Renato Cavalcanti <renato@cavalcanti.be>
2023-05-03 15:53:30 +0200

    feat: run docker-compose from kalix:runAll

---
+++ maven-java/kalix-maven-plugin/src/main/scala/kalix/RunAllMojo.scala
@@ -0,0 +1,32 @@
+package kalix
+
+import kalix.devtools.impl.DockerComposeUtils
+import org.apache.maven.plugins.annotations.Execute
+import org.apache.maven.plugins.annotations.LifecyclePhase
+import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.ResolutionScope
+
+/**
+ * Runs the current project. This goal will by default start a Kalix Server (Proxy) and the current application.
+ */
+@Mojo(name = "runAll", defaultPhase = LifecyclePhase.VALIDATE, requiresDependencyResolution = ResolutionScope.RUNTIME)
+@Execute(phase = LifecyclePhase.COMPILE)
+class RunAllMojo extends RunParameters with DockerParameters {
+
+  override def execute(): Unit = {
+
+    val dockerComposeUtils = DockerComposeUtils(dockerComposeFile, sys.env)
+    dockerComposeUtils.start()
+
+    RunMojo(
+      mainClass,
+      logConfig,
+      dockerComposeUtils.readUserFunctionPort,
+      dockerComposeUtils.readServicePortMappings,
+      getLog,
+      mavenProject,
+      mavenSession,
+      pluginManager,
+      runningSolo = false)
+  }
+}
---
+++ maven-java/kalix-maven-plugin/src/main/scala/kalix/RunParameters.scala
@@ -0,0 +1,27 @@
+package kalix
+
+import org.apache.maven.execution.MavenSession
+import org.apache.maven.plugin.AbstractMojo
+import org.apache.maven.plugin.BuildPluginManager
+import org.apache.maven.plugins.annotations.Component
+import org.apache.maven.plugins.annotations.Parameter
+import org.apache.maven.project.MavenProject
+
+trait RunParameters extends AbstractMojo {
+
+  @Component
+  protected var mavenProject: MavenProject = null
+
+  @Component
+  protected var mavenSession: MavenSession = null
+
+  @Component
+  protected var pluginManager: BuildPluginManager = null
+
+  @Parameter(property = "mainClass")
+  protected var mainClass: String = ""
+
+  @Parameter(property = "kalix.dev-mode.log-config", defaultValue = "src/main/resources/logback-dev-mode.xml")
+  protected var logConfig: String = ""
+
+}
