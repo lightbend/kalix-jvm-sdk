deb96a5971c3161ac7a9d0baf5d24f512b5f4e49
Enno Runne <458526+ennru@users.noreply.github.com>
2023-05-03 17:56:52 +0200

    No newline in echo

--- bin/check-sample-protoc-version.sh
+++ bin/check-sample-protoc-version.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 AKKA_GRPC_PROTOC_FULL=$(sbt "print coreSdk/protocVersion" 2>/dev/null | tail -n 2)
 echo "full Akka gRPC version text: \"\"\"${AKKA_GRPC_PROTOC_FULL}\"\"\""
-AKKA_GRPC_PROTOC=$(echo $AKKA_GRPC_PROTOC_FULL | head -n 1)
+AKKA_GRPC_PROTOC=$(echo -n $AKKA_GRPC_PROTOC_FULL | head -n 1)
 POM_PROTOC_VERSIONS=$(find ./samples ./maven-java/ -name pom.xml | xargs egrep -h "com.google.protobuf:protoc:" | sed 's/.*:protoc://' | sed 's/:exe.*//' | sort | uniq)
 if [ $(echo "$POM_PROTOC_VERSIONS" | wc -l) -ne 1 ]; then
   echo  "Not all Java Samples an Archetypes have the same protoc version:"
