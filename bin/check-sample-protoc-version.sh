7f1e10b1eadb5b808ffa37b3e0af510d658eff0e
Enno Runne <458526+ennru@users.noreply.github.com>
2023-05-04 14:30:05 +0200

    different approach to cut and paste

--- bin/check-sample-protoc-version.sh
+++ bin/check-sample-protoc-version.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-AKKA_GRPC_PROTOC_FULL=$(sbt "print coreSdk/protocVersion" 2>/dev/null | tail -n 2)
-echo "full Akka gRPC version text: \"\"\"${AKKA_GRPC_PROTOC_FULL}\"\"\""
-AKKA_GRPC_PROTOC=$(echo -n $AKKA_GRPC_PROTOC_FULL | head -n 1)
+AKKA_GRPC_PROTOC_FULL=$(sbt --client "print coreSdk/protocVersion" 1>/tmp/kalix-version.txt 2>/dev/null)
+# the tail distance is different locally and on CI
+AKKA_GRPC_PROTOC=$(cat /tmp/kalix-version.txt | tail -n 2 | head -n 1)
 POM_PROTOC_VERSIONS=$(find ./samples ./maven-java/ -name pom.xml | xargs egrep -h "com.google.protobuf:protoc:" | sed 's/.*:protoc://' | sed 's/:exe.*//' | sort | uniq)
 if [ $(echo "$POM_PROTOC_VERSIONS" | wc -l) -ne 1 ]; then
   echo  "Not all Java Samples an Archetypes have the same protoc version:"
@@ -11,5 +11,6 @@ if [ $(echo "$POM_PROTOC_VERSIONS" | wc -l) -ne 1 ]; then
   false
 elif [ "$AKKA_GRPC_PROTOC" != "$POM_PROTOC_VERSIONS" ]; then
   echo  "Java Samples and Archetypes ($POM_PROTOC_VERSIONS) does not have the same protoc version as Akka gRPC ($AKKA_GRPC_PROTOC)"
+  echo "DEBUG: full Akka gRPC version text: \"\"\"$(cat /tmp/kalix-version.txt)\"\"\""
   false
 fi
