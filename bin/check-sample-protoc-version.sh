ea7e58e9ea91af7ce59e92662549f0ee371a2c7b
Enno Runne <458526+ennru@users.noreply.github.com>
2023-05-04 14:45:25 +0200

    One line above?

--- bin/check-sample-protoc-version.sh
+++ bin/check-sample-protoc-version.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 AKKA_GRPC_PROTOC_FULL=$(sbt --client "print coreSdk/protocVersion" 1>/tmp/kalix-version.txt 2>/dev/null)
 # the tail distance is different locally and on CI
-AKKA_GRPC_PROTOC=$(cat /tmp/kalix-version.txt | tail -n 2 | head -n 1)
+AKKA_GRPC_PROTOC=$(cat /tmp/kalix-version.txt | tail -n 3 | head -n 1)
 POM_PROTOC_VERSIONS=$(find ./samples ./maven-java/ -name pom.xml | xargs egrep -h "com.google.protobuf:protoc:" | sed 's/.*:protoc://' | sed 's/:exe.*//' | sort | uniq)
 if [ $(echo "$POM_PROTOC_VERSIONS" | wc -l) -ne 1 ]; then
   echo  "Not all Java Samples an Archetypes have the same protoc version:"
