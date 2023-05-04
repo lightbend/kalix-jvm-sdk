5c8d1af984debbe35402991ff469b56993714245
Enno Runne <458526+ennru@users.noreply.github.com>
2023-05-04 15:18:19 +0200

    Don't use native client, no colors

--- bin/check-sample-protoc-version.sh
+++ bin/check-sample-protoc-version.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-AKKA_GRPC_PROTOC_FULL=$(sbt --client "print coreSdk/protocVersion" 1>/tmp/kalix-version.txt 2>/dev/null)
+AKKA_GRPC_PROTOC_FULL=$(sbt -no-colors "print coreSdk/protocVersion" 1>/tmp/kalix-version.txt 2>/dev/null)
 # the tail distance is different locally and on CI
-AKKA_GRPC_PROTOC=$(cat /tmp/kalix-version.txt | tail -n 3 | head -n 1 | xargs)
+AKKA_GRPC_PROTOC=$(cat /tmp/kalix-version.txt | tail -n 1 | head -n 1 | xargs)
 POM_PROTOC_VERSIONS=$(find ./samples ./maven-java/ -name pom.xml | xargs egrep -h "com.google.protobuf:protoc:" | sed 's/.*:protoc://' | sed 's/:exe.*//' | sort | uniq)
 if [ $(echo "$POM_PROTOC_VERSIONS" | wc -l) -ne 1 ]; then
   echo  "Not all Java Samples an Archetypes have the same protoc version:"
