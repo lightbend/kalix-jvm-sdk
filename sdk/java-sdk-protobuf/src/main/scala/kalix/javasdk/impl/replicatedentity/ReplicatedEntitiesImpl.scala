3f003c9b3a561ff5052e1def37c9d8d9174108c7
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2023-05-10 13:50:16 +0200

    fmt

--- sdk/java-sdk-protobuf/src/main/scala/kalix/javasdk/impl/replicatedentity/ReplicatedEntitiesImpl.scala
+++ sdk/java-sdk-protobuf/src/main/scala/kalix/javasdk/impl/replicatedentity/ReplicatedEntitiesImpl.scala
@@ -24,7 +24,7 @@ import kalix.javasdk.impl.effect.{ EffectSupport, ErrorReplyImpl, MessageReplyIm
 import kalix.javasdk.impl.replicatedentity.ReplicatedEntityEffectImpl.DeleteEntity
 import kalix.javasdk.impl.replicatedentity.ReplicatedEntityRouter.CommandResult
 import kalix.javasdk.replicatedentity._
-import kalix.javasdk.{ Metadata }
+import kalix.javasdk.Metadata
 import kalix.protocol.entity.Command
 import kalix.protocol.replicated_entity.ReplicatedEntityStreamIn.{ Message => In }
 import kalix.protocol.replicated_entity.ReplicatedEntityStreamOut.{ Message => Out }
