899c3be27d8ec801af80590115aaddaf23113748
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2023-06-26 15:37:51 +0200

    reverting test changes

--- samples/java-spring-fibonacci-action/src/it/java/com/example/fibonacci/FibonacciActionIntegrationTest.java
+++ samples/java-spring-fibonacci-action/src/it/java/com/example/fibonacci/FibonacciActionIntegrationTest.java
@@ -1,26 +1,17 @@
 package com.example.fibonacci;
 
 import com.example.Main;
-import com.google.protobuf.any.Any;
-import kalix.javasdk.DeferredCall;
-import kalix.javasdk.client.ComponentClient;
 import kalix.spring.testkit.KalixIntegrationTestKitSupport;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
 
@@ -31,18 +22,16 @@ public class FibonacciActionIntegrationTest extends KalixIntegrationTestKitSuppo
   @Autowired
   private WebClient webClient;
 
-  @Autowired
-  private ComponentClient componentClient;
-  private Duration timeout = Duration.of(5, SECONDS);
-
   @Test
   public void calculateNextNumber() {
 
-    Number response = execute(componentClient.forAction()
-        .call(FibonacciAction::getNumber)
-        .params(5L));
+    Mono<Number> response =
+      webClient.get()
+        .uri("/fibonacci/5/next")
+        .retrieve().bodyToMono(Number.class);
 
-    Assertions.assertEquals(8, response.value());
+    long next = response.block(Duration.of(5, SECONDS)).value();
+    Assertions.assertEquals(8, next);
 
   }
 
@@ -75,14 +64,6 @@ public class FibonacciActionIntegrationTest extends KalixIntegrationTestKitSuppo
     } catch (WebClientResponseException.InternalServerError ex) {
       String bodyErrorMessage = ex.getResponseBodyAsString();
       Assertions.assertTrue(bodyErrorMessage.contains("Input number is not a Fibonacci number"));
-    }
-  }
-
-  private <T> T execute(DeferredCall<Any, T> deferredCall) {
-    try {
-      return deferredCall.execute().toCompletableFuture().get(timeout.toMillis(), TimeUnit.MILLISECONDS);
-    } catch (InterruptedException | ExecutionException | TimeoutException e) {
-      throw new RuntimeException(e);
     }
   }
 }
