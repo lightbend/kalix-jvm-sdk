06dc9c5775d399fc4a37c39ddfbc161f8d7d24bf
Eduardo Pinto <eduardo.pinto@lightbend.com>
2023-09-05 23:14:58 +0100

    docs(java): inject jwt token in integration tests

--- samples/java-spring-doc-snippets/src/main/java/com/example/jwt/JWTAction.java
+++ samples/java-spring-doc-snippets/src/main/java/com/example/jwt/JWTAction.java
@@ -1,5 +1,6 @@
 package com.example.jwt;
 
+import kalix.javasdk.StatusCode;
 import kalix.javasdk.action.Action;
 import kalix.javasdk.annotations.JWT;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +15,25 @@ public class JWTAction extends Action {
             return effects().reply(msg);
         }
     // end::bearer-token[]
+
     // tag::bearer-token-issuer[]
     @PostMapping("/message")
     @JWT(validate = JWT.JwtMethodMode.BEARER_TOKEN,
-     bearerTokenIssuer = "my-issuer")       // <1>
+      bearerTokenIssuer = "my-issuer")       // <1>
     public Action.Effect<String> messageWithIssuer(@RequestBody String msg) {
         return effects().reply(msg);
     }
     // end::bearer-token-issuer[]
+
+    // tag::bearer-token-claims[]
+    @PostMapping("/message")
+    @JWT(validate = JWT.JwtMethodMode.BEARER_TOKEN)
+    public Action.Effect<String> messageWithClaimValidation(@RequestBody String msg) {
+        var maybeSubject = actionContext().metadata().jwtClaims().subject();
+        if (maybeSubject.isEmpty())
+            return effects().error("No subject present", StatusCode.ErrorCode.UNAUTHORIZED);
+
+        return effects().reply(msg);
+    }
+    // end::bearer-token-claims[]
 }
