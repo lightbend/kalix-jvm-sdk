102f95c25f4c9d8456d55bfc20655784d04e0b04
Renato Cavalcanti <renato@cavalcanti.be>
2023-10-26 13:39:02 +0200

    fix: remove thread local based wiring (#1817)
    * fix: remove thread local based wiring
    
    * added missing test for ill-defined Workflow
    
    * throwing exceptions for real
    
    * fix contructor validation

---
+++ sdk/java-sdk-spring/src/test/java/kalix/spring/badwiring/workflow/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 Lightbend Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kalix.spring.badwiring.workflow;
+
+import kalix.spring.boot.KalixConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(KalixConfiguration.class)
+public class Main {
+
+  private static final Logger logger = LoggerFactory.getLogger(kalix.spring.badwiring.view.Main.class);
+
+  public static void main(String[] args) {
+    logger.info("Starting Kalix Application");
+    SpringApplication.run(kalix.spring.badwiring.view.Main.class, args);
+  }
+}
