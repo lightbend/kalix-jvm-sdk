caf315f08d2c502e930b5db46674636eab6c207d
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-11-15 12:44:35 +0100

    kalix-proxy protocol 1.1.23

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Kalix {
     val ProtocolVersionMajor = 1
     val ProtocolVersionMinor = 1
-    val ProxyVersion = System.getProperty("kalix-proxy.version", "1.1.22-8-7d112bc6-dev-SNAPSHOT")
+    val ProxyVersion = System.getProperty("kalix-proxy.version", "1.1.23")
   }
 
   // changing the Scala version of the Java SDK affects end users
