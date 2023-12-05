17ad8b687e68179c99b63d71aa7fe009af11788e
Enno Runne <458526+ennru@users.noreply.github.com>
2023-12-05 15:56:17 +0100

    Found even more, sigh

--- devtools/src/test/scala/kalix/devtools/impl/DevModeSettingsSpec.scala
+++ devtools/src/test/scala/kalix/devtools/impl/DevModeSettingsSpec.scala
@@ -27,8 +27,8 @@ object DevModeSettingsSpec {
     """
       |version: "3"
       |services:
-      |  kalix-proxy:
-      |    image: gcr.io/kalix-public/kalix-runtime:1.1.24
+      |  kalix-runtime:
+      |    image: gcr.io/kalix-public/kalix-runtime:1.1.25
       |    ports:
       |      - "9000:9000"
       |    extra_hosts:
