b516d0729fc6a8ef17c5f0c179e1e5453d93d2cb
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2024-01-23 15:00:34 +0100

    using pre released version of the runtime

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val RuntimeVersion = System.getProperty(
       "kalix-runtime.version",
       // temporarily accept the old system property name
-      System.getProperty("kalix-proxy.version", "v1.1.29-10-620db190"))
+      System.getProperty("kalix-proxy.version", "1.1.29-10-620db190"))
   }
 
   // changing the Scala version of the Java SDK affects end users
