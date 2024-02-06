7a50bc7de9dfb73b855c5ae9161c234220746e44
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2024-02-06 15:38:14 +0100

    Added code from the ComponentsSourceGenerator to the tests

--- codegen/java-gen/src/test/resources/tests/replicated-entity/multimap-unnamed-new-style/generated-managed/org/example/ComponentsImpl.java
+++ codegen/java-gen/src/test/resources/tests/replicated-entity/multimap-unnamed-new-style/generated-managed/org/example/ComponentsImpl.java
@@ -47,7 +47,7 @@ public final class ComponentsImpl implements Components {
     public DeferredCall<com.example.replicated.multimap.SomeMultiMapApi.PutValue, com.google.protobuf.Empty> put(com.example.replicated.multimap.SomeMultiMapApi.PutValue putValue) {
       return new GrpcDeferredCall<>(
         putValue,
-        MetadataImpl.Empty(),
+        (MetadataImpl) ((InternalContext) context).componentGrpcClientMetadata(),
         "com.example.replicated.multimap.MultiMapService",
         "Put",
         (Metadata metadata) -> {
