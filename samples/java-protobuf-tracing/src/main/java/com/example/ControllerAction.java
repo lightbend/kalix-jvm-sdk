4a9fc69308fe6ee2b913898b11c8704c99d360fe
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2024-03-25 12:27:57 +0700

    addressing comments
    - removing sync service
    - adding jackson mapper to show only the title of the post
    - removing params from the application.conf to pom.xml

--- samples/java-protobuf-tracing/src/main/java/com/example/ControllerAction.java
+++ samples/java-protobuf-tracing/src/main/java/com/example/ControllerAction.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Charsets;
 import com.google.protobuf.Empty;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.StatusCode;
@@ -12,6 +15,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
@@ -24,87 +28,39 @@ public class ControllerAction extends AbstractControllerAction {
 
 
   @Override
-  public Effect<ControllerActionApi.MessageResponse> callSyncEndpoint(Empty empty) {
-    //Taking the already configured tracer. Such it will know where to export the spans
-    Optional<Tracer> tracerOpt = actionContext().getOpenTelemetryTracer();
-    String result;
-
-    if (tracerOpt.isPresent()){
-      result = callSyncService(tracerOpt.get());
-    } else {
-      try {
-        HttpResponse<String> response = callSyncService();
-        result = response.body();
-      } catch (IOException | InterruptedException e){
-        result = e.getMessage();
-      }
-    }
-    return effects().reply(ControllerActionApi.MessageResponse.newBuilder().setMessage(result).build());
-  }
-
-  private  HttpResponse<String> callSyncService() throws IOException, InterruptedException {
-    HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(url))
-            .build();
-    return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-  }
-
-  private String callSyncService(Tracer tracer) {
-    Span span = tracer
-            .spanBuilder("loreipsumendpoint")
-            .setParent(actionContext().metadata().traceContext().asOpenTelemetryContext())
-            .startSpan();
-    span.setAttribute("attribute1", "value1");
-
-    String result = "";
-    //scope must be closed. try-with-resources will automatically close it.
-    try (Scope scope = span.makeCurrent()) {
-      //Sync call to external service
-      HttpResponse<String> response = callSyncService();
-      result = response.body();
-      span.setAttribute("result", result);
-    } catch (IOException | InterruptedException e) {
-      result = e.getMessage();
-      span.setStatus(StatusCode.ERROR, result);
-    } finally {
-      span.end();
-    }
-    return result;
-  }
-
-
-    @Override
   public Effect<ControllerActionApi.MessageResponse> callAsyncEndpoint(Empty empty) {
       // tag::get-tracer[]
       Optional<Tracer> tracerOpt = actionContext().getOpenTelemetryTracer();
       // end::get-tracer[]
-      CompletableFuture<HttpResponse<String>> futureResponse;
+      CompletableFuture<HttpResponse<Post>> futureResponse;
 
     if(tracerOpt.isPresent()) {
       futureResponse = callAsyncService(tracerOpt.get());
     } else {
       futureResponse = callAsyncService();
     }
-    CompletionStage<ControllerActionApi.MessageResponse> asyncComputation = futureResponse.toCompletableFuture().thenApply(response -> {
-      return ControllerActionApi.MessageResponse.newBuilder().setMessage(response.body()).build();
+    CompletionStage<ControllerActionApi.MessageResponse> asyncComputation = futureResponse.toCompletableFuture()
+            .thenApply(response -> {
+      return ControllerActionApi.MessageResponse.newBuilder().setMessage(response.body().title).build();
     });
     return effects().asyncReply(asyncComputation);
   }
 
 
-  private CompletableFuture<HttpResponse<String>> callAsyncService(){
+  private CompletableFuture<HttpResponse<Post>> callAsyncService(){
     HttpRequest httpRequest = HttpRequest.newBuilder()
             .uri(URI.create(url))
             .build();
     //Async call to external service
     return httpClient.sendAsync(httpRequest,
-            HttpResponse.BodyHandlers.ofString());
+            new JsonResponseHandler<>(Post.class));
+
 
   }
 
 
   //  tag::create-close-span[]
-  private CompletableFuture<HttpResponse<String>> callAsyncService(Tracer tracer) {
+  private CompletableFuture<HttpResponse<Post>> callAsyncService(Tracer tracer) {
   // end::create-close-span[]
 
     // tag::create-close-span[]
@@ -114,14 +70,15 @@ public class ControllerAction extends AbstractControllerAction {
             .startSpan(); // <2>
     span.setAttribute("attribute1", "value1");// <3>
 
-    CompletableFuture<HttpResponse<String>> responseFuture = callAsyncService();
+    CompletableFuture<HttpResponse<Post>> responseFuture = callAsyncService();
 
     try (Scope scope = span.makeCurrent()) {// <4>
       responseFuture.thenAccept(response -> {
-        String responseBody = response.body();
-        span.setAttribute("result", responseBody);// <5>
+        span.setAttribute("result", response.body().title);// <5>
       }).exceptionally(ex -> {
         span.setStatus(StatusCode.ERROR, ex.getMessage());
+        System.out.println("########### so far so good callAsyncService");
+
         return null;
       });
     } finally {
@@ -131,6 +88,8 @@ public class ControllerAction extends AbstractControllerAction {
   }
   // end::create-close-span[]
 
+
+  public record Post(String userId, String id, String title, String body) {}
 
 
 
