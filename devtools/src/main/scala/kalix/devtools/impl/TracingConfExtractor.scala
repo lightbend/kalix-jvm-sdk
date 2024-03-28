7719f8ed95d873d795ae2e9cfc314fb20daf7f10
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2024-03-28 11:48:24 +0700

    added usage of tracing config

--- devtools/src/main/scala/kalix/devtools/impl/TracingConfExtractor.scala
+++ devtools/src/main/scala/kalix/devtools/impl/TracingConfExtractor.scala
@@ -1,25 +1,40 @@
+/*
+ * Copyright 2024 Lightbend Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package kalix.devtools.impl
 
 import com.typesafe.config.ConfigFactory
 
 object TracingConfExtractor {
-  /**
-   * Extract the [[DevModeSettings.userFunctionTracingKey]] from a line and returns them as a Seq without
-   * the settings prefix.
-   */
+
+  //TODO make sure this is needed
   def unapply(line: String): Option[TracingConfig] = {
     val tracingConfig =
       line
-      .split("-D")
-      .foldLeft(TracingConfig.empty) { case (acc, curr) =>
-        curr match {
-          case curr if curr.startsWith(DevModeSettings.tracingConfigEnabled) =>
-            acc.copy(enabled = ConfigFactory.parseString(curr).getBoolean(DevModeSettings.tracingConfigEnabled))
-          case curr if curr.startsWith(DevModeSettings.tracingConfigEndpoint) =>
-            acc.copy(collectorEndpoint = ConfigFactory.parseString(curr).getString(DevModeSettings.tracingConfigEndpoint))
-          case _ => acc
+        .split("-D")
+        .foldLeft(TracingConfig.empty) { case (acc, curr) =>
+          curr match {
+            case curr if curr.startsWith(DevModeSettings.tracingConfigEnabled) =>
+              acc.copy(enabled = ConfigFactory.parseString(curr).getBoolean(DevModeSettings.tracingConfigEnabled))
+            case curr if curr.startsWith(DevModeSettings.tracingConfigEndpoint) =>
+              acc.copy(collectorEndpoint =
+                Some(ConfigFactory.parseString(curr).getString(DevModeSettings.tracingConfigEndpoint)))
+            case _ => acc
+          }
         }
-      }
     if (tracingConfig.isEmpty) None
     else {
       Some(tracingConfig)
@@ -28,9 +43,9 @@ object TracingConfExtractor {
 
 }
 
-case class TracingConfig(enabled: Boolean, collectorEndpoint: String) {
-  def isEmpty: Boolean = !enabled && collectorEndpoint == ""
+case class TracingConfig(enabled: Boolean, collectorEndpoint: Option[String]) {
+  def isEmpty: Boolean = !enabled && collectorEndpoint.isEmpty
 }
 object TracingConfig {
-  def empty: TracingConfig =  TracingConfig(enabled = false, "")
+  def empty: TracingConfig = TracingConfig(enabled = false, None)
 }
