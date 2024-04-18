8c4181ba3828a92977bd7edd0b9fa99d91bc2320
Enno Runne <458526+ennru@users.noreply.github.com>
2024-04-18 14:14:43 +0200

    Publishing to Maven (Take 3) (#2093)

--- .github/publish-maven.sh
+++ .github/publish-maven.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 echo "Extracting the SDK version from sbt build"
-SDK_VERSION=$(sbt "print coreSdk/version" | tail -n 1)
-IS_SNAPSHOT=$(sbt "print coreSdk/isSnapshot"  | tail -n 1)
-if [ "true" == "${IS_SNAPSHOT}" ]
+SDK_VERSION=$(sbt --no-colors "print coreSdk/version" | tail -n 1 | tr -d '\n')
+IS_SNAPSHOT=$(sbt --no-colors "print coreSdk/isSnapshot" | tail -n 1 | tr -d '\n')
+if [ "false" == "${IS_SNAPSHOT}" ]
 then
-  REPO="akka-repo::default::https://maven.cloudsmith.io/lightbend/akka-snapshots/"
-else
   REPO="akka-repo::default::https://maven.cloudsmith.io/lightbend/akka/"
+else
+  REPO="akka-repo::default::https://maven.cloudsmith.io/lightbend/akka-snapshots/"
 fi
 echo "Publishing ${SDK_VERSION} to ${REPO}"
+
+cd maven-java
 echo ${PGP_SECRET} | base64 -d | gpg --import --batch
 mkdir -p ~/.m2
 cat <<EOF >~/.m2/settings.xml
