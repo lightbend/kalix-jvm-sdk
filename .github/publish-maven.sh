f084c7eae6a98ebacef2f8dfbd931778da5567c3
Enno Runne <458526+ennru@users.noreply.github.com>
2024-04-18 16:21:32 +0200

    Extract sbt props in client mode (#2101)

--- .github/publish-maven.sh
+++ .github/publish-maven.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Extracting the SDK version from sbt build"
-SDK_VERSION=$(sbt --client --no-colors "print coreSdk/version" | tail -n 1 | tr -d '\n')
-IS_SNAPSHOT=$(sbt --client --no-colors "print coreSdk/isSnapshot" | tail -n 1 | tr -d '\n')
+SDK_VERSION=$(sbt --client --no-colors "print coreSdk/version" | tail -n 3 | head -n 1 | tr -d '\n')
+IS_SNAPSHOT=$(sbt --client --no-colors "print coreSdk/isSnapshot" | tail -n 3 | head -n 1 | tr -d '\n')
 if [ "false" == "${IS_SNAPSHOT}" ]
 then
   REPO="akka-repo::default::https://maven.cloudsmith.io/lightbend/akka/"
 else
   REPO="akka-repo::default::https://maven.cloudsmith.io/lightbend/akka-snapshots/"
 fi
-echo "Publishing ${SDK_VERSION} to ${REPO}"
+echo "Publishing '${SDK_VERSION}' to '${REPO}'"
 
 cd maven-java
 echo ${PGP_SECRET} | base64 -d | gpg --import --batch
