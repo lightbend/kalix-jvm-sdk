6c78abb1af893a45dfcc635afaaabd1fb749d97c
Enno Runne <458526+ennru@users.noreply.github.com>
2024-04-22 11:39:33 +0200

    Repositories in archetypes, debug publishing (#2113)

--- .github/determine-sdk-version.sh
+++ .github/determine-sdk-version.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
-set -e
-
 echo "Extracting version from sbt build"
 # deliberately not using `--client`
-sbt --no-colors "print coreSdk/version" > sdk-version-raw.txt
+sbt --no-colors "print coreSdk/version" | tail -n 1 | tr -d '\n' > sdk-version.txt
 # debugging help
-echo "----"
-cat sdk-version-raw.txt
-echo "----"
-cat sdk-version-raw.txt | tail -n 1 | tr -d '\n' > sdk-version.txt
-# debugging help
-echo "----"
 hexdump -c sdk-version.txt
-echo "----"
