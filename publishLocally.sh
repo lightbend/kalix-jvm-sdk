1e187863e2d9176be93d4bfedce13f5d0e72aae0
Renato Cavalcanti <renato@cavalcanti.be>
2024-04-23 16:59:11 +0200

    feat: kalix-spring-parent pom (#2117)

--- publishLocally.sh
+++ publishLocally.sh
@@ -12,6 +12,17 @@ sbt 'publishM2; publishLocal'
 (
   cd maven-java
   mvn versions:set -DnewVersion=$SDK_VERSION
+
+  (
+    # special case for parent pom, version:set only changes project sharing the same parent
+    # kalix-spring-boot-parent is special because its parent is spring-boot-starter-parent
+    cd kalix-spring-boot-parent
+    mvn versions:set -DnewVersion=$SDK_VERSION
+
+    # also needs to change kalix-sdk.version in parent pom
+    sed "s/<kalix-sdk.version>\(.*\)<\/kalix-sdk.version>/<kalix-sdk.version>$SDK_VERSION<\/kalix-sdk.version>/" pom.xml
+  )
+
   mvn clean install
 
   # cleanup
