f8e8a353ca1a69ec96bee518963b6e84d6fc899e
Renato Cavalcanti <renato@cavalcanti.be>
2024-04-25 14:04:11 +0200

    fix: enable all parent pom plugins

--- publishLocally.sh
+++ publishLocally.sh
@@ -1,7 +1,7 @@
 # This script will publish the current snapshot of all artifacts. 
 # Including the maven plugin and archetypes.
 
-SDK_VERSION=$(sbt "print javaSdkProtobuf/version" | tail -1)
+export SDK_VERSION=$(sbt "print coreSdk/version" | tail -1)
 
 echo
 echo "------------------------------------------------------------------------"
@@ -11,21 +11,8 @@ echo "------------------------------------------------------------------------"
 sbt 'publishM2; publishLocal'
 (
   cd maven-java
-  mvn --activate-profiles patch-version versions:set -DnewVersion=$SDK_VERSION
-  
-  ( # also needs to change kalix-sdk.version in parent pom
-    cd kalix-java-protobuf-parent
-    sed -i.bak "s/<kalix-sdk.version>\(.*\)<\/kalix-sdk.version>/<kalix-sdk.version>$SDK_VERSION<\/kalix-sdk.version>/" pom.xml
-    rm pom.xml.bak
-  )
-
-  ( # also needs to change kalix-sdk.version in parent pom
-    cd kalix-spring-boot-parent
-    sed -i.bak "s/<kalix-sdk.version>\(.*\)<\/kalix-sdk.version>/<kalix-sdk.version>$SDK_VERSION<\/kalix-sdk.version>/" pom.xml
-    rm pom.xml.bak
-  )
-
-  mvn clean install
+  ../.github/patch-maven-versions.sh
+  mvn clean install -Dskip.docker=true
 
   # cleanup
   rm pom.xml.versionsBackup
