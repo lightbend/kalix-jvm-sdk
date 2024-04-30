bfc1b6b14c5c4368b40d25afefe29674da4dae7e
Renato Cavalcanti <renato@cavalcanti.be>
2024-04-30 16:02:22 +0200

    chore: kalix-sdk-version.txt everywhere

--- .github/determine-sdk-version.sh
+++ .github/determine-sdk-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Extracting version from sbt build"
 # deliberately not using `--client`
-sbt --no-colors "print coreSdk/version" | tail -n 1 | tr -d '\n' > sdk-version.txt
+sbt --no-colors "print coreSdk/version" | tail -n 1 | tr -d '\n' > ~/kalix-sdk-version.txt
 # debugging help
-hexdump -c sdk-version.txt
+hexdump -c ~/kalix-sdk-version.txt
