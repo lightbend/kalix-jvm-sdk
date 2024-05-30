fc45802431ff15b2b31258e6ac4767fdddc14b9c
Eduardo Pinto <eduardo.pinto@lightbend.com>
2024-05-30 20:45:44 +0100

    bump: update sdk version to 1.5.0 (#2153)

--- updateSdkVersions.sh
+++ updateSdkVersions.sh
@@ -49,16 +49,13 @@ case ${option} in
       ;;
    plugin) updateMavenPlugin
       ;;
-   docs) updateDocs
-      ;;
    all)
-     updateDocs
      updateJavaSamples $sample
      updateScalaSamples $sample
      updateMavenPlugin
       ;;
    *)
-      echo "`basename ${0}`:usage: java|scala|plugin|docs|all [project-folder]"
+      echo "`basename ${0}`:usage: java|scala|plugin|all [project-folder]"
       echo "e.g.: `basename ${0}` java ./samples/java-protobuf-customer-registry-kafka-quickstart/"
       exit 1 # Command to come out of the program with status 1
       ;;
