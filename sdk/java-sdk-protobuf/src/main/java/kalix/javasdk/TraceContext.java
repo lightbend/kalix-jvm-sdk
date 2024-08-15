4c7875f87ca668a82179c2bf579f725ffcd12245
Francisco Lopez-Sancho <franciscolopezsancho@gmail.com>
2024-08-15 17:09:13 +0200

    java fmt

--- sdk/java-sdk-protobuf/src/main/java/kalix/javasdk/TraceContext.java
+++ sdk/java-sdk-protobuf/src/main/java/kalix/javasdk/TraceContext.java
@@ -41,7 +41,8 @@ public interface TraceContext {
 
   /**
    * @return the traceId of the traceParent if any, otherwise "00000000000000000000000000000000"
-   * @see <a href="https://github.com/open-telemetry/opentelemetry-java/blob/ad120a5bff0887dffedb9c73af8e8e0aeb63659a/api/all/src/main/java/io/opentelemetry/api/trace/TraceId.java#L3"
+   * @see <a
+   *     href="https://github.com/open-telemetry/opentelemetry-java/blob/ad120a5bff0887dffedb9c73af8e8e0aeb63659a/api/all/src/main/java/io/opentelemetry/api/trace/TraceId.java#L3"
    */
   String traceId();
 }
