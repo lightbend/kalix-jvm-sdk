6ecfb8ce6e955f2a9e28ac21102933d766d32319
Johan Andrén <johan@markatta.com>
2025-03-05 14:37:39 +0100

    build: Stop building docker image for TCK
    # Conflicts:
    #	bin/docker-login-public-gcr.sh
    #	project/DockerImage.scala

--- bin/docker-login-public-gcr.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# This script configures docker to be able to push to Google Container Registry.
-# It uses the service account associated with PUBLIC_GCR_SERVICE_KEY.
-
-set -euo pipefail
-
-# FIXME need to be about publishing to or own repo
-echo "$KALIX_PUBLIC_GCR_SERVICE_KEY" | docker login -u _json_key --password-stdin https://gcr.io
