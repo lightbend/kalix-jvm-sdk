9ce758ff6b22d5a659f3de1d56de872270ecf3e8
Eduardo Pinto <efgpinto@gmail.com>
2025-03-13 13:07:49 +0000

    note about pinned version

--- tck/scala-tck/src/test/java/kalix/javasdk/tck/RunTck.java
+++ tck/scala-tck/src/test/java/kalix/javasdk/tck/RunTck.java
@@ -18,6 +18,7 @@ import scala.concurrent.Await;
 import scala.concurrent.duration.Duration;
 
 public final class RunTck {
+  // pinned to last released version as we don't plan to release new versions of the TCK
   public static final String TCK_IMAGE = "registry.akka.io/kalix-tck";
   public static final String TCK_VERSION = "1.2.11";
 
