da4d9432f5a01214223fb829d1ff9c46a3ea21c9
Justin Pihony <justin.pihony@gmail.com>
2025-10-15 07:39:21 -0400

    More resolver (#2367)
    * Handle merging of global maven settings
    * Update publish-maven.sh

--- .github/publish-maven.sh
+++ .github/publish-maven.sh
@@ -30,9 +30,29 @@ fi
 cd maven-java
 ../.github/patch-maven-versions.sh
 
-# create Maven settings.xml with credentials for repository publishing
-mkdir -p ~/.m2
-cat <<EOF >~/.m2/settings.xml
+# create or merge Maven settings.xml with credentials for repository publishing
+MVN_SETTINGS_FILE="$HOME/.m2/settings.xml"
+mkdir -p "$(dirname "$MVN_SETTINGS_FILE")"
+
+if [ -f "$XML_FILE" ]; then
+    echo "⚙️ Settings file found. Merging publishing configuration..."
+    xmlstarlet ed -L \
+        -s /settings -t elem -n servers -v "" \
+        -s /settings/servers[1] -t elem -n server \
+        -i /settings/servers[1]/server[1] -t attr -n id -v central \
+        -s /settings/servers[1]/server[1] -t elem -n username -v '${SONATYPE_USERNAME}' \
+        -s /settings/servers[1]/server[1] -t elem -n password -v '${SONATYPE_PASSWORD}' \
+        "$MVN_SETTINGS_FILE"
+    xmlstarlet ed -L \
+        -s '/settings/profiles/profile[id="akka-repo"]' -t elem -n activation -v "" \
+        -s '/settings/profiles/profile[id="akka-repo"]/activation' -t elem -n activeByDefault -v "true" \
+        -s '/settings/profiles/profile[id="akka-repo"]' -t elem -n properties -v "" \
+        -s '/settings/profiles/profile[id="akka-repo"]/properties' -t elem -n gpg.passphrase -v '${PGP_PASSPHRASE}' \
+        "$MVN_SETTINGS_FILE"
+    echo "✅ Merge complete."
+else
+    echo "✨ Settings file not found. Creating a new one with publishing configuration."
+    cat <<EOF >"$MVN_SETTINGS_FILE"
 <settings xmlns="http://maven.apache.org/SETTINGS/1.1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.1.0 http://maven.apache.org/xsd/settings-1.1.0.xsd">
   <servers>
@@ -55,6 +75,8 @@ cat <<EOF >~/.m2/settings.xml
   </profiles>
 </settings>
 EOF
+    echo "✅ File created."
+fi
 
 # import the artefact signing key
 echo "${PGP_SECRET}" | base64 -d | gpg --import --batch
